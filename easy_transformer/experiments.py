0d92f8b09c6b7ba3e7892916f67d8b3243ec088c
Arthur Conmy <arthurconmy@gmail.com>
2022-09-03 23:55:06 -0700

    docs

--- easy_transformer/experiments.py
+++ easy_transformer/experiments.py
@@ -261,6 +261,12 @@ class EasyExperiment:
 
 
 class EasyAblation(EasyExperiment):
+    """
+    Run an ablation experiment according to the config object
+    Pass semantic_indices not None to average across different index positions 
+    (probably limited used currently, see test_experiments for one usage)
+    """
+
     def __init__(self, model: EasyTransformer, config: AblationConfig, metric: ExperimentMetric, semantic_indices = None):
         super().__init__(model, config, metric)
         assert "AblationConfig" in str(type(config))
