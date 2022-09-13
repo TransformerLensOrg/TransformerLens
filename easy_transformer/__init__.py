ff45168c00eca7dbf4ec748897a6db537b3f093d
Ansh Radhakrishnan <anshuman.radhakrishnan@yale.edu>
2022-09-13 15:14:51 -0700

    Working greedy_decoding and sampling with cache

--- easy_transformer/__init__.py
+++ easy_transformer/__init__.py
@@ -2,5 +2,9 @@ from . import hook_points
 from . import EasyTransformer
 from . import experiments
 from . import utils
-from . import EasyTransformerConfig
+from .EasyTransformerConfig import EasyTransformerConfig
+from .EasyTransformerKeyValueCache import (
+    EasyTransformerKeyValueCache,
+    EasyTransformerKeyValueCacheEntry,
+)
 from .EasyTransformer import EasyTransformer
