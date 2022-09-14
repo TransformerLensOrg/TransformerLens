e2b7e627b078ac2eb5bd0a1d66f2063d24de6bd2
Arthur Conmy <arthurconmy@gmail.com>
2022-09-13 22:02:34 -0700

    add the minimality styff

--- ioi_minimality.py
+++ ioi_minimality.py
@@ -84,9 +84,7 @@ if ipython is not None:
 model_name = "gpt2"  # Here we used gpt-2 small ("gpt2")
 
 print_gpu_mem("About to load model")
-model = EasyTransformer(
-    model_name, use_attn_result=True
-)  # use_attn_result adds a hook blocks.{lay}.attn.hook_result that is before adding the biais of the attention layer
+model = EasyTransformer(model_name, use_attn_result=True)
 device = "cuda"
 if torch.cuda.is_available():
     model.to(device)
@@ -94,13 +92,10 @@ print_gpu_mem("Gpt2 loaded")
 # %% [markdown]
 # Each prompts is a dictionnary containing 'IO', 'S' and the "text", the sentence that will be given to the model.
 # The prompt type can be "ABBA", "BABA" or "mixed" (half of the previous two) depending on the pattern you want to study
-
-
 # %%
 # IOI Dataset initialisation
 N = 200
 ioi_dataset = IOIDataset(prompt_type="mixed", N=N, tokenizer=model.tokenizer)
-
 # %%
 webtext = load_dataset("stas/openwebtext-10k")
 owb_seqs = [
@@ -111,9 +106,7 @@ owb_seqs = [
     )
     for i in range(ioi_dataset.N)
 ]
-
 #%%
-
 from ioi_circuit_extraction import (
     join_lists,
     CIRCUIT,
@@ -155,33 +148,16 @@ def logit_diff(model, ioi_dataset, all=False, std=False):
 
 
 #%% [markdown]
-# TODO Explain the way we're doing Jacob's circuit extraction experiment here
-#%% # basis change experiments
-circuit_perf_scatter = []
-circuit_perf = []
+# TODO Explain the way we're doing the minimal circuit experiment here
+#%%
 
+results = {}
 
-def basis_change(x, y):
-    """
-    Change the basis (1, 0) and (0, 1) to the basis
-    1/sqrt(2) (1, 1) and 1/sqrt(2) (-1, 1)
-    """
-
-    return (x + y) / np.sqrt(2), (y - x) / np.sqrt(2)
-
-
-for G in tqdm(list(CIRCUIT.keys()) + ["none"]):
-    if G == "ablation":
-        continue
+for G in tqdm(list(CIRCUIT.keys())):
     print_gpu_mem(G)
-    # compute METRIC( C \ G )
-    # excluded_classes = ["calibration"]
-    excluded_classes = []
-    if G != "none":
-        excluded_classes.append(G)
-    heads_to_keep = get_heads_circuit(
-        ioi_dataset, excluded_classes=excluded_classes
-    )  # TODO check the MLP stuff
+
+    # compute METRIC(C \ W)
+    heads_to_keep = get_heads_circuit(ioi_dataset, excluded_classes=[G])
     torch.cuda.empty_cache()
 
     model.reset_hooks()
@@ -192,72 +168,35 @@ for G in tqdm(list(CIRCUIT.keys()) + ["none"]):
         ioi_dataset=ioi_dataset,
     )
     torch.cuda.empty_cache()
-    ldiff_broken_circuit, std_broken_circuit = logit_diff(
-        model, ioi_dataset, std=True, all=True
-    )
-    torch.cuda.empty_cache()
-    # metric(C\G)
-    # adding back the whole model
-
-    excl_class = list(CIRCUIT.keys())
-    if G != "none":
-        excl_class.remove(G)
-    G_heads_to_remove = get_heads_circuit(
-        ioi_dataset, excluded_classes=excl_class
-    )  # TODO check the MLP stuff
+    ldiff_broken_circuit, std_broken_circuit = logit_diff(model, ioi_dataset, std=True)
     torch.cuda.empty_cache()
 
-    model.reset_hooks()
-    model, _ = do_circuit_extraction(
-        model=model,
-        heads_to_remove=G_heads_to_remove,
-        mlps_to_remove={},
-        ioi_dataset=ioi_dataset,
-    )
-    torch.cuda.empty_cache()
-    ldiff_cobble, std_cobble_circuit = logit_diff(
-        model, ioi_dataset, std=True, all=True
-    )
-    torch.cuda.empty_cache()
+    results[G] = {
+        "ldiff_broken_circuit": ldiff_broken_circuit,
+        "std_broken_circuit": std_broken_circuit,
+    }
+    results[G]["vs"] = {}
 
-    # metric(M\G)
+    # METRIC((C \ W) \cup \{ v \})
 
-    on_diagonals = []
-    off_diagonals = []
+    for v in tqdm(list(CIRCUIT[G].keys())):
+        new_heads_to_keep = heads_to_keep.copy()
+        v_indices = get_extracted_idx(RELEVANT_TOKENS[v], ioi_dataset)
+        assert v not in new_heads_to_keep.keys()
+        new_heads_to_keep[v] = v_indices
 
-    for i in range(len(ldiff_cobble)):
-        circuit_perf_scatter.append(
-            {
-                "removed_group": G,
-                "ldiff_broken": ldiff_broken_circuit[i],
-                "ldiff_cobble": ldiff_cobble[i],
-                "sentence": ioi_dataset.text_prompts[i],
-                "template": ioi_dataset.templates_by_prompt[i],
-            }
+        model.reset_hooks()
+        model, _ = do_circuit_extraction(
+            model=model,
+            heads_to_keep=new_heads_to_keep,
+            mlps_to_remove={},
+            ioi_dataset=ioi_dataset,
         )
+        torch.cuda.empty_cache()
+        ldiff_with_v = logit_diff(model, ioi_dataset, std=True)
+        results[G]["vs"][v] = ldiff_with_v
+        torch.cuda.empty_cache()
 
-        x, y = basis_change(
-            circuit_perf_scatter[-1]["ldiff_broken"],
-            circuit_perf_scatter[-1]["ldiff_cobble"],
-        )
-        circuit_perf_scatter[-1]["on_diagonal"] = x
-        circuit_perf_scatter[-1]["off_diagonal"] = y
-        on_diagonals.append(x)
-        off_diagonals.append(y)
-
-    circuit_perf.append(
-        {
-            "removed_group": G,
-            "ldiff_broken": ldiff_broken_circuit.detach().mean(),
-            "ldiff_cobble": ldiff_cobble.detach().mean(),
-            "std_ldiff_broken": std_broken_circuit,
-            "std_ldiff_cobble": std_cobble_circuit,
-            "on_diagonal": np.mean(on_diagonals),
-            "off_diagonal": np.mean(off_diagonals),
-            "std_on_diagonal": np.std(on_diagonals),
-            "std_off_diagonal": np.std(off_diagonals),
-        }
-    )
 #%%
 fig = px.scatter(
     circuit_perf,
