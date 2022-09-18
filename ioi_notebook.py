2bb08e87c6efb8c277e63bf50d48269a28d9be02
Arthur Conmy <arthurconmy@gmail.com>
2022-09-18 15:48:45 -0700

    ...things broke?

--- ioi_notebook.py
+++ ioi_notebook.py
@@ -1086,11 +1086,6 @@ for ablate_negative in [
     True,
 ]:
     ld_data = []
-    score_data = []
-    probs_data = []
-    sprobs_data = []
-    io_logits_data = []
-    s_logits_data = []
     for template_idx in tqdm(range(num_templates)):
         prompts = template_prompts[template_idx]
         ioi_dataset = IOIDataset(
@@ -1104,6 +1099,9 @@ for ablate_negative in [
                 ioi_dataset.sem_tok_idx[key] == idx
             ), f"{key} {ioi_dataset.sem_tok_idx[key]}"
             # check that semantic ablation = normal ablation
+
+        model.reset_hooks()
+        ld_initial = logit_diff(model, ioi_dataset)
 
         model.reset_hooks()  # TODO find other instances of model deletion
         seq_len = ioi_dataset.toks.shape[1]
@@ -1121,172 +1119,8 @@ for ablate_negative in [
             mlps_to_keep={},
         )
 
-        if False:  # surely there's a better way to do this...
-            head_indices_to_ablate = {
-                (i % 12, i // 12): [
-                    list(range(seq_len)) for _ in range(len(ioi_dataset.text_prompts))
-                ]
-                for i in range(12 * 12)
-            }
-
-            mlp_indices_to_ablate = [[] for _ in range(model.cfg.n_heads)]
-
-            for head in [
-                (0, 1),
-                (0, 10),
-                (3, 0),
-            ]:
-                head_indices_to_ablate[head] = [
-                    i for i in range(seq_len) if i != ioi_dataset.sem_tok_idx["S2"][0]
-                ]
-
-            for head in [
-                (4, 11),
-                (2, 2),
-                (2, 9),
-            ]:
-                head_indices_to_ablate[head] = [
-                    i
-                    for i in range(seq_len)
-                    if i
-                    not in [
-                        ioi_dataset.sem_tok_idx["S"][0],
-                        ioi_dataset.sem_tok_idx["and"][0],
-                    ]
-                ]
-
-            for head in [
-                (5, 8),
-                (5, 9),
-                (5, 5),
-                (6, 9),
-            ]:
-                head_indices_to_ablate[head] = [
-                    i
-                    for i in range(seq_len)
-                    if i not in [ioi_dataset.sem_tok_idx["S2"][0]]
-                ]
-
-            end_heads = [
-                (7, 3),
-                (7, 9),
-                (8, 6),
-                (8, 10),
-                (9, 6),
-                (9, 9),
-                (10, 0),
-            ]
-
-            if not ablate_calibration:
-                end_heads += [(10, 7), (11, 12)]
-
-            for head in end_heads:
-                head_indices_to_ablate[head] = [
-                    i
-                    for i in range(seq_len)
-                    if i not in [ioi_dataset.sem_tok_idx["end"][0]]
-                ]
-
-        # define the ablation function for ALL parts of the model at once
-        def ablation(z, mean, hook):
-            layer = int(hook.name.split(".")[1])
-            head_idx = hook.ctx["idx"]
-            head = (layer, head_idx)
-
-            if "mlp_out" in hook.name:
-                # ablate the relevant parts
-                for i in range(z.shape[0]):
-                    z[i, mlp_indices_to_ablate[layer]] = mean[
-                        i, mlp_indices_to_ablate[layer]
-                    ].to(z.device)
-
-            if (
-                "attn.hook_result" in hook.name
-            ):  # and (layer, head) not in heads_to_keep:
-                # ablate
-                assert (
-                    len(z.shape) == 3
-                ), z.shape  # we specifically get sent the relevant head
-                assert 12 not in list(
-                    z.shape
-                ), "Yikes, probably dim kept back is wrong, should be head dim"
-
-                # see above
-                for i in range(z.shape[0]):
-                    z[i, head_indices_to_ablate[head]] = mean[
-                        i, head_indices_to_ablate[head]
-                    ].to(z.device)
-
-            return z
-
-        ld_metric = ExperimentMetric(
-            metric=logit_diff_target, dataset=ioi_dataset, relative_metric=False
-        )
-        score_metric = ExperimentMetric(
-            metric=score, dataset=ioi_dataset, relative_metric=False
-        )
-        ld_metric.set_baseline(model)
-        score_metric.set_baseline(model)
-        probs_metric = ExperimentMetric(
-            metric=io_probs, dataset=ioi_dataset, relative_metric=False
-        )
-        probs_metric.set_baseline(model)
-        sprobs_metric = ExperimentMetric(
-            metric=lambda x, y: io_probs(x, y, mode="S"),
-            dataset=ioi_dataset,
-            relative_metric=False,
-        )
-        sprobs_metric.set_baseline(model)
-        io_logits_metric = ExperimentMetric(
-            metric=lambda x, y: logit_diff_target(x, y, all=True)[0],
-            dataset=ioi_dataset,
-            relative_metric=False,
-        )
-        io_logits_metric.set_baseline(model)
-        s_logits_metric = ExperimentMetric(
-            metric=lambda x, y: logit_diff_target(x, y, all=True)[1],
-            dataset=ioi_dataset,
-            relative_metric=False,
-        )
-        s_logits_metric.set_baseline(model)
-
-        config = AblationConfig(
-            abl_type="custom",
-            abl_fn=ablation,
-            mean_dataset=ioi_dataset.text_prompts,
-            target_module="attn_head",
-            head_circuit="result",
-            cache_means=True,
-            verbose=True,
-        )
-
-        abl = EasyAblation(
-            model, config, ld_metric
-        )  # semantic_indices=ioi_dataset.sem_tok_idx] # semantic indices should not be necessary
-
-        model.reset_hooks()
-        for layer in range(12):
-            for head in range(12):
-                model.add_hook(*abl.get_hook(layer, head))
-            model.add_hook(*abl.get_hook(layer, head=None, target_module="mlp"))
-
-        # compute a bunch of datasets
-        ld = ld_metric.compute_metric(model)
-        ld_data.append((ld_metric.baseline, ld))
-        cur_score = score_metric.compute_metric(model)
-        score_data.append((score_metric.baseline, cur_score))
-        cur_probs = probs_metric.compute_metric(model)
-        probs_data.append((probs_metric.baseline, cur_probs))
-
-        s_probs = sprobs_metric.compute_metric(
-            model
-        )  # s probs is like 0.003 for most ablate calibration heads # or is is that low
-        # print(f"{s_probs=}")
-        sprobs_data.append((sprobs_metric.baseline, s_probs))
-        io_logits = io_logits_metric.compute_metric(model)
-        io_logits_data.append((io_logits_metric.baseline, io_logits))
-        s_logits = s_logits_metric.compute_metric(model)
-        s_logits_data.append((s_logits_metric.baseline, s_logits))
+        ld_final = logit_diff(model, ioi_dataset)
+        ld_data.append((ld_initial, ld_final))
 
     xs = [ld_data[i][0].item() for i in range(num_templates)]
     ys = [ld_data[i][1].item() for i in range(num_templates)]
