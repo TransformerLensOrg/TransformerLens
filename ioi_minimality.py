a198e0db80c298d05c036f137faf58456cb19553
Arthur Conmy <arthurconmy@gmail.com>
2022-10-07 12:52:28 -0700

    more sadly screwed experiments

--- ioi_minimality.py
+++ ioi_minimality.py
@@ -551,20 +551,22 @@ for j in range(2, 5):
         # print(some_atts.shape)
 
         prior_stuff = []
-        for i in range(-1, 2):
-            
-        prior_stuff.append(z[torch.arange(ioi_dataset.N), s2_positions, s_positions - 1  + i].clone())
+        for i in range(0, 2):
+            prior_stuff.append(z[torch.arange(ioi_dataset.N), s2_positions, s_positions + i].clone())
+        for i in range(0, 2):
+            z[torch.arange(ioi_dataset.N), s2_positions, s_positions + i] =  prior_stuff[(i + j) % 2] # +1 is the do nothing one # ([0, 1][(i+j)%2]) is way beyond scope
 
-        # for i in range(-1, 2):
-            # z[torch.arange(ioi_dataset.N), s2_positions, s_positions - 1 + i] = prior_stuff[(i + j) % 3]  # +1 is the do nothing one
+        # z[torch.arange(ioi_dataset.N), s2_positions, s_positions] = 0 
+        # z[torch.arange(ioi_dataset.N), s2_positions, s_positions + 1] = 1
+        # z[torch.arange(ioi_dataset.N), s2_positions, 0] = 0
 
         return z
 
     model.reset_hooks()
     ld = logit_diff(model, ioi_dataset)
 
-    circuit_classes = ["induction"]
-    # circuit_classes = ["duplicate token"]
+    # circuit_classes = ["induction"]
+    circuit_classes = ["duplicate token"]
     # circuit_classes = ["duplicate token", "induction"]
     # circuit_classes = ["previous token"]
 
