34b28f94665da44746e5d795cb484613670709d9
ArthurConmy <arthurconmy@gmail.com>
2022-10-23 10:59:07 -0700

    oooh now we make the direct patch and freeaze

--- ioi_notebook.py
+++ ioi_notebook.py
@@ -176,7 +176,11 @@ ioi_dataset_abba = IOIDataset(prompt_type="ABBA", N=N, tokenizer=model.tokenizer
 ioi_dataset = IOIDataset(prompt_type="mixed", N=N, tokenizer=model.tokenizer, prepend_bos=True, has_start_padding_and_start_is_end=True)
 abca_dataset = ioi_dataset.gen_flipped_prompts(("S2", "RAND"))  # we flip the second b for a random c
 pprint(abca_dataset.text_prompts[:5])
-
+all_diff_dataset = (
+    ioi_dataset.gen_flipped_prompts(("IO", "RAND"))
+    .gen_flipped_prompts(("S", "RAND"))
+    .gen_flipped_prompts(("S1", "RAND"), manual_word_idx=ioi_dataset.word_idx)
+)
 abca_dataset_abba = ioi_dataset_abba.gen_flipped_prompts(("S2", "RAND"))
 abca_dataset_baba = ioi_dataset_baba.gen_flipped_prompts(("S2", "RAND"))
 
@@ -2154,4 +2158,227 @@ all_prods = {f"{layer}.{head}": prod[layer, head, :] for (layer, head) in import
 
 px.line(all_prods, labels={"index": "Position in sequence of random tokens", "value": "Dot product"})
 
+#%% [markdown] look at logit writing for the GPT NEO
+
+def patch_positions(z, source_act, hook, positions=["end"]):
+    for pos in positions:
+        z[torch.arange(ioi_dataset.N), ioi_dataset.word_idx[pos]] = source_act[
+            torch.arange(ioi_dataset.N), ioi_dataset.word_idx[pos]
+        ]
+    return z
+
+
+def patch_all(z, source_act, hook):
+    return source_act
+
+
+def direct_patch_and_freeze(
+    model,
+    source_dataset,
+    target_dataset,
+    ioi_dataset,
+    sender_heads,
+    receiver_hooks,
+    max_layer,
+    positions=["end"],
+    verbose=False,
+    return_hooks=False,
+    extra_hooks=[],  # when we call reset hooks, we may want to add some extra hooks after this, add these here
+):
+    """
+    Patch in the effect of `sender_heads` on `receiver_hooks` only
+    (though MLPs are "ignored", so are slight confounders)
+
+    If max_layer < 12, then let some part of the model do computations (not frozen)
+    """
+
+    sender_hooks = []
+
+    for layer, head_idx in sender_heads:
+        if head_idx is None:
+            sender_hooks.append((f"blocks.{layer}.hook_mlp_out", None))
+
+        else:
+            sender_hooks.append((f"blocks.{layer}.attn.hook_result", head_idx))
+
+    sender_hook_names = [x[0] for x in sender_hooks]
+    receiver_hook_names = [x[0] for x in receiver_hooks]
+
+    sender_cache = {}
+    model.reset_hooks()
+    for hook in extra_hooks:
+        model.add_hook(*hook)
+    model.cache_some(sender_cache, lambda x: x in sender_hook_names)
+    # print(f"{sender_hook_names=}")
+    source_logits = model(
+        source_dataset.text_prompts
+    )  # this should see what the logits are when i) main heads are ablated + ii) we're also ablating (lay, head_idx)
+
+    target_cache = {}
+    model.reset_hooks()
+    for hook in extra_hooks:
+        model.add_hook(*hook)
+    model.cache_all(target_cache)
+    target_logits = model(target_dataset.text_prompts)
+
+    # for all the Q, K, V things
+    model.reset_hooks()
+    for layer in range(max_layer):
+        for head_idx in range(12):
+            for hook_template in [
+                "blocks.{}.attn.hook_q",
+                "blocks.{}.attn.hook_k",
+                "blocks.{}.attn.hook_v",
+            ]:
+                hook_name = hook_template.format(layer)
+
+                if hook_name in receiver_hook_names:
+                    continue
+
+                hook = get_act_hook(
+                    patch_all,
+                    alt_act=target_cache[hook_name],
+                    idx=head_idx,
+                    dim=2 if head_idx is not None else None,
+                    name=hook_name,
+                )
+                model.add_hook(hook_name, hook)
+    for hook in extra_hooks:
+        # ughhh, think that this is what we want, this should override the QKV above
+        model.add_hook(*hook)
+
+    # we can override the hooks above for the sender heads, though
+    for hook_name, head_idx in sender_hooks:
+        assert not torch.allclose(sender_cache[hook_name], target_cache[hook_name]), (
+            hook_name,
+            head_idx,
+        )
+        hook = get_act_hook(
+            partial(patch_positions, positions=positions),
+            alt_act=sender_cache[hook_name],
+            idx=head_idx,
+            dim=2 if head_idx is not None else None,
+            name=hook_name,
+        )
+        model.add_hook(hook_name, hook)
+
+    # measure the receiver heads' values
+    receiver_cache = {}
+    model.cache_some(receiver_cache, lambda x: x in receiver_hook_names)
+    receiver_logits = model(target_dataset.text_prompts)
+
+    # patch these values in
+    model.reset_hooks()
+    for hook in extra_hooks:
+        model.add_hook(
+            *hook
+        )  # ehh probably doesn't actually matter cos end thing hooked
+
+    hooks = []
+    for hook_name, head_idx in receiver_hooks:
+        hook = get_act_hook(
+            partial(patch_positions, positions=positions),
+            alt_act=receiver_cache[hook_name],
+            idx=head_idx,
+            dim=2 if head_idx is not None else None,
+            name=hook_name,
+        )
+        hooks.append((hook_name, hook))
+
+    if return_hooks:
+        return hooks
+    else:
+        for hook_name, hook in hooks:
+            model.add_hook(hook_name, hook)
+        return model
+
+
+#%% [markdown] first patch-and-freeze experiments
+# TODO why are there effects that come AFTER the patching?? it's before 36 mins in voko I think
+
+dataset_names = [
+    # "ioi_dataset",
+    # "abca_dataset",
+    # "dcc_dataset",
+    # "acca_dataset",
+    # "acba_dataset",
+    "all_diff_dataset",
+    # "totally_diff_dataset",
+]
+
+# patch all heads into the name mover input (hopefully find S2 Inhibition)
+
+exclude_heads = [(layer, head_idx) for layer in range(12) for head_idx in range(12)]
+for head in [(9, 9), (9, 6), (10, 0)]:
+    exclude_heads.remove(head)
+
+model.reset_hooks()
+default_logit_diff = logit_diff(model, ioi_dataset)
+
+# for pos in ["S+1", "S", "IO", "S2", "end"]:
+for pos in ["end"]:
+    print(pos)
+    results = torch.zeros(size=(12, 12))
+    mlp_results = torch.zeros(size=(12, 1))
+    for source_layer in tqdm(range(12)):
+        for source_head_idx in [None] + list(range(12)):
+            model.reset_hooks()
+            receiver_hooks = []
+            # for layer, head_idx in circuit["name mover"]:
+            # receiver_hooks.append((f"blocks.{layer}.attn.hook_q", head_idx))
+            # receiver_hooks.append((f"blocks.{layer}.attn.hook_v", head_idx))
+            # receiver_hooks.append((f"blocks.{layer}.attn.hook_k", head_idx))
+            receiver_hooks.append(("blocks.11.hook_resid_post", None))
+
+            model = direct_patch_and_freeze(
+                model=model,
+                source_dataset=all_diff_dataset,
+                target_dataset=ioi_dataset,
+                ioi_dataset=ioi_dataset,
+                sender_heads=[(source_layer, source_head_idx)],
+                receiver_hooks=receiver_hooks,
+                max_layer=12,
+                positions=[pos],
+                verbose=False,
+                return_hooks=False,
+            )
+
+            cur_logit_diff = logit_diff(model, ioi_dataset)
+
+            if source_head_idx is None:
+                mlp_results[source_layer] = cur_logit_diff - default_logit_diff
+            else:
+                results[source_layer][source_head_idx] = (
+                    cur_logit_diff - default_logit_diff
+                )
+
+            if source_layer == 11 and source_head_idx == 11:
+                # show attention head results
+                fname = f"svgs/patch_and_freeze_{pos}_{ctime()}_{ri(2134, 123759)}"
+                fig = show_pp(
+                    results.T,
+                    title=f"{fname=} {pos=} patching NMs",
+                    return_fig=True,
+                    show_fig=False,
+                )
+
+                fig.write_image(
+                    f"svgs/patch_and_freezes/to_duplicate_token_K_{pos}.png"
+                )
+
+                fig.write_image(fname + ".png")
+                fig.write_image(fname + ".svg")
+                fig.show()
+
+                # # show mlp results # mlps are (hopefully not anymore???) fucked
+                fname = f"svgs/patch_and_freeze_mlp_{ctime()}_{ri(2134, 123759)}"
+                fig = show_pp(
+                    mlp_results.T,
+                    title="Direct effect of MLPs on Logit Difference",
+                    return_fig=True,
+                    show_fig=False,
+                )
+                fig.write_image(fname + ".png")
+                fig.write_image(fname + ".svg")
+                fig.show()
 # %%
