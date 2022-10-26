90b3d22fcc7cbbbc01f5261688effada35202af5
ArthurConmy <arthurconmy@gmail.com>
2022-10-26 14:05:54 -0700

    with no internal interaction s thyins are great

--- real_notebook.py
+++ real_notebook.py
@@ -165,14 +165,14 @@ print(
 model.reset_hooks()
 default_logit_diff = logit_diff(model, ioi_dataset)
 
-for pos in ["S+1"]:
+for pos in ["S2"]:
     results = torch.zeros(size=(12, 12))
     mlp_results = torch.zeros(size=(12, 1))
     for source_layer in tqdm(range(12)):
         for source_head_idx in [None] + list(range(12)):
             model.reset_hooks()
             receiver_hooks = []
-            for layer, head_idx in circuit["induction"]:
+            for layer, head_idx in circuit["s2 inhibition"]:
                 # receiver_hooks.append((f"blocks.{layer}.attn.hook_q", head_idx))
                 # receiver_hooks.append((f"blocks.{layer}.attn.hook_v", head_idx))
                 receiver_hooks.append((f"blocks.{layer}.attn.hook_k", head_idx))
@@ -192,7 +192,7 @@ for pos in ["S+1"]:
                 verbose=False,
                 return_hooks=False,
                 freeze_mlps=False,
-                have_internal_interactions=True,
+                have_internal_interactions=False,
             )
             cur_logit_diff = logit_diff(model, ioi_dataset)
 
