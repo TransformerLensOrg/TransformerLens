19354b462fa2ec215387d8c01392cfc9bfaba04b
Haoxing Du <haoxingdu@gmail.com>
2022-11-03 05:39:38 +0000

    code now runs recursively

--- circuit_discovery.py
+++ circuit_discovery.py
@@ -1,8 +1,7 @@
 # %%
-from utils_circuit_discovery import get_hook_tuple
+from utils_circuit_discovery import get_hook_tuple, path_patching, path_patching_up_to, logit_diff_io_s
 from ioi_utils import show_pp
 from functools import partial
-#import plotly.graph_objects as go
 import numpy as np
 from typing import List, Tuple, Dict, Union, Optional, Callable, Any
 from tqdm import tqdm
@@ -12,6 +11,8 @@ import plotly.express as px
 import gc
 import einops
 from collections import OrderedDict
+from ioi_dataset import IOIDataset
+
 
 from easy_transformer import EasyTransformer
 from easy_transformer.experiments import get_act_hook
@@ -29,152 +30,11 @@ if torch.cuda.is_available():
 model.set_use_attn_result(True)
 
 # %%
-
-def patch_all(z, source_act, hook):
-    return source_act
-
-def patch_positions(z, source_act, hook, positions):
-    if positions is None: # same as patch_all
-        raise NotImplementedError("haven't implemented not specifying positions to patch")
-        # return source_act
-    else:
-        batch = z.shape[0]
-        for pos in positions:
-            z[torch.arange(batch), pos] = source_act[torch.arange(batch), pos]
-        return z
-
-def path_patching(
-    model: EasyTransformer, 
-    orig_data, 
-    new_data, 
-    senders: List[Tuple], 
-    receiver_hooks: List[Tuple], 
-    max_layer: Union[int, None] = None, 
-    position: int = 0,
-    return_hooks: bool = False,
-    freeze_mlps: bool = True):
-    """ mlps are by default considered as just another component and so are
-        by default frozen when collecting acts on receivers. 
-        orig_data: string, torch.Tensor, or list of strings - any format that can be passed to the model directly
-        new_data: same as orig_data
-        senders: list of tuples (layer, head) for attention heads and (layer, None) for mlps
-        receiver_hooks: list of tuples (hook_name, head) for attn heads and (hook_name, None) for mlps
-        max_layer: layers beyond max_layer are not frozen when collecting receiver activations
-        positions: default None and patch at all positions, or a tensor specifying the positions at which to patch
-
-        NOTE: This relies on a change to the cache_some() function in EasyTransformer/hook_points.py.
-    """
-    if max_layer is None:
-        max_layer = model.cfg.n_layers
-    assert max_layer <= model.cfg.n_layers
-
-    # save activations from orig
-    orig_cache = {}
-    model.cache_all(orig_cache)
-    _ = model(orig_data)
-    model.reset_hooks()
-
-    # process senders
-    sender_hooks = []
-    for layer, head in senders:
-        if head is not None: # (layer, head) for attention heads
-            sender_hooks.append((f"blocks.{layer}.attn.hook_result", head))
-        else: # (layer, None) for mlps
-            sender_hooks.append((f"blocks.{layer}.hook_mlp_out", None))
-    sender_hook_names = [x[0] for x in sender_hooks]
-
-    # save activations from new for senders
-    new_cache = {}
-    model.cache_some(new_cache, lambda x: x in sender_hook_names)
-    _ = model(new_data)
-    model.reset_hooks()
-
-    # set up receiver cache
-    receiver_hook_names = [x[0] for x in receiver_hooks]
-    receiver_cache = {}
-    model.cache_some(receiver_cache, lambda x: x in receiver_hook_names)
-
-    # configure hooks for freezing activations
-    for layer in range(max_layer):
-        # heads
-        for head in range(model.cfg.n_heads):
-            # if (layer, head) in senders:
-            #     continue
-            for hook_template in [
-                'blocks.{}.attn.hook_q',
-                'blocks.{}.attn.hook_k',
-                'blocks.{}.attn.hook_v',
-            ]:
-                hook_name = hook_template.format(layer)
-                hook = get_act_hook(
-                    patch_all,
-                    alt_act=orig_cache[hook_name],
-                    idx=head,
-                    dim=2,
-                )
-                model.add_hook(hook_name, hook)
-        # mlp
-        if freeze_mlps:
-            hook_name = f'blocks.{layer}.hook_mlp_out'
-            hook = get_act_hook(
-                patch_all,
-                alt_act=orig_cache[hook_name],
-                idx=None,
-                dim=None,
-            )
-            model.add_hook(hook_name, hook)
-    
-    # for senders, add new hook to patching in new acts
-    for hook_name, head in sender_hooks:
-        #assert not torch.allclose(orig_cache[hook_name], new_cache[hook_name]), (hook_name, head)
-        hook = get_act_hook(
-            partial(patch_positions, positions=[position]),
-            alt_act=new_cache[hook_name],
-            idx=head,
-            dim=2 if head is not None else None,
-        )
-        model.add_hook(hook_name, hook)
-    
-    # forward pass on orig, where patch in new acts for senders and orig acts for the rest
-    # and save activations on receivers
-    _ = model(orig_data)
-    model.reset_hooks()
-
-    # add hooks for final forward pass on orig, where we patch in hybrid acts for receivers
-    hooks = []
-    for hook_name, head in receiver_hooks:
-        #assert not torch.allclose(orig_cache[hook_name], receiver_cache[hook_name])
-        hook = get_act_hook(
-            partial(patch_positions, positions=[position]),
-            alt_act=receiver_cache[hook_name],
-            idx=head,
-            dim=2 if head is not None else None,
-        )
-        hooks.append((hook_name, hook))
-    
-    if return_hooks:
-        return hooks
-    else:
-        for hook_name, hook in hooks:
-            model.add_hook(hook_name, hook)
-        return model
-
-# %%
 orig = "When John and Mary went to the store, John gave a bottle of milk to Mary."
 new = "When John and Mary went to the store, Charlie gave a bottle of milk to Mary."
 #new = "A completely different gibberish sentence blalablabladfghjkoiuytrdfg"
 
 logit = model(orig)[0,16,5335]
-
-# model = path_patching(
-#     model, 
-#     orig, 
-#     new, 
-#     [(8, 6)],
-#     [('blocks.9.attn.hook_q', 9)],
-#     12,
-#     positions=[torch.tensor([16])],
-# )
 
 model = path_patching(
     model, 
@@ -183,29 +43,16 @@ model = path_patching(
     [(5, 5)],
     [('blocks.8.attn.hook_v', 6)],
     12,
-    positions=[torch.tensor([16])],
+    position=torch.tensor([16]),
 )
-
-# model = path_patching(
-#     model, 
-#     orig, 
-#     new, 
-#     [(9, 9)],
-#     [('blocks.11.hook_resid_post', None)],
-#     12,
-#     positions=[torch.tensor([16])],
-# )
 
 new_logit = model(orig)[0,16,5335]
 model.reset_hooks()
 print(logit, new_logit)
 
 # %%
-from ioi_dataset import (
-    IOIDataset,
-)
 
-N = 1
+N = 50
 ioi_dataset = IOIDataset(
     prompt_type="mixed",
     N=N,
@@ -227,53 +74,9 @@ abc_dataset = (
 # 3. for comp in identified components:
 ## a. run path patching on all components upstream to it, with the q, k, or v part of comp as receiver
 
-def path_patching_up_to(
-    model: EasyTransformer, 
-    layer: int,
-    metric, 
-    dataset,
-    orig_data, 
-    new_data, 
-    receiver_hooks,
-    position
-):
-    model.reset_hooks()
-    attn_results = np.zeros((layer, model.cfg.n_heads))
-    mlp_results = np.zeros((layer,1))
-    for l in tqdm(range(layer)):
-        for h in range(model.cfg.n_heads):
-            model = path_patching(
-                model,
-                orig_data=orig_data,
-                new_data=new_data,
-                senders=[(l, h)],
-                receiver_hooks=receiver_hooks,
-                max_layer=model.cfg.n_layers,
-                position=position
-            )
-            attn_results[l, h] = metric(model, dataset)
-            model.reset_hooks()
-        # mlp
-        model = path_patching(
-            model,
-            orig_data=orig_data,
-            new_data=new_data,
-            senders=[(l, None)],
-            receiver_hooks=receiver_hooks,
-            max_layer=model.cfg.n_layers,
-            position=position
-        )
-        mlp_results[l] = metric(model, dataset)
-        model.reset_hooks()
-    return attn_results, mlp_results
-
-def logit_diff_io_s(model: EasyTransformer, dataset: IOIDataset):
-    N = dataset.N
-    io_logits = model(dataset.toks.long())[torch.arange(N), dataset.word_idx['end'], dataset.io_tokenIDs]
-    s_logits = model(dataset.toks.long())[torch.arange(N), dataset.word_idx['end'], dataset.s_tokenIDs]
-    return (io_logits - s_logits).mean().item()
     
 # %%
+# Try out path patching at residual stream
 receiver_hooks = [('blocks.11.hook_resid_post', None)]
 attn_results, mlp_results = path_patching_up_to(
     model, 
@@ -285,7 +88,6 @@ attn_results, mlp_results = path_patching_up_to(
     receiver_hooks=receiver_hooks,
     positions=[ioi_dataset.word_idx['end']])
 
-# %%
 model.reset_hooks()
 default_logit_diff = logit_diff_io_s(model, ioi_dataset)
 attn_results_n = (attn_results - default_logit_diff) / default_logit_diff
@@ -294,7 +96,13 @@ px.imshow(attn_results_n, color_continuous_scale='RdBu', color_continuous_midpoi
 px.imshow(np.expand_dims(mlp_results_n, axis=0), color_continuous_scale='RdBu', color_continuous_midpoint=0)
 
 # %%
-receiver_hooks = [('blocks.9.attn.hook_q', 9)]#, ('blocks.9.attn.hook_q', 6), ('blocks.10.attn.hook_q', 0)]
+# Name mover queries
+#receiver_hooks = [('blocks.9.attn.hook_q', 9), ('blocks.9.attn.hook_q', 6), ('blocks.10.attn.hook_q', 0)]
+receiver_hooks = [
+    ('blocks.11.attn.hook_q', 10), 
+    ('blocks.11.attn.hook_k', 10), 
+    ('blocks.11.attn.hook_v', 10), ]
+    #('blocks.10.attn.hook_q', 7)]
 attn_results, mlp_results = path_patching_up_to(
     model, 
     layer=10, 
@@ -303,16 +111,17 @@ attn_results, mlp_results = path_patching_up_to(
     orig_data=ioi_dataset.toks.long(),
     new_data=abc_dataset.toks.long(),
     receiver_hooks=receiver_hooks,
-    positions=[ioi_dataset.word_idx['end']])
-# %%
+    position=ioi_dataset.word_idx['end'])
+
 model.reset_hooks()
 default_logit_diff = logit_diff_io_s(model, ioi_dataset)
 attn_results_n = (attn_results - default_logit_diff) / default_logit_diff
 mlp_results_n = (mlp_results - default_logit_diff) / default_logit_diff
 px.imshow(attn_results_n, color_continuous_scale='RdBu', color_continuous_midpoint=0).show()
-px.imshow(np.expand_dims(mlp_results_n, axis=0), color_continuous_scale='RdBu', color_continuous_midpoint=0)
+px.imshow(mlp_results_n, color_continuous_scale='RdBu', color_continuous_midpoint=0)
 
 # %%
+# S-inhibition heads
 receiver_hooks = [
     ('blocks.7.attn.hook_v', 3), 
     ('blocks.7.attn.hook_v', 9), 
@@ -321,14 +130,13 @@ receiver_hooks = [
 attn_results, mlp_results = path_patching_up_to(
     model, 
     layer=10, 
-
     metric=logit_diff_io_s,
     dataset=ioi_dataset,
     orig_data=ioi_dataset.toks.long(),
     new_data=abc_dataset.toks.long(),
     receiver_hooks=receiver_hooks,
-    positions=ioi_dataset.word_idx['S2'])
-# %%
+    position=ioi_dataset.word_idx['S2'])
+
 attn_results_n = (attn_results - default_logit_diff) / default_logit_diff
 mlp_results_n = (mlp_results - default_logit_diff) / default_logit_diff
 px.imshow(attn_results_n, color_continuous_scale='RdBu', color_continuous_midpoint=0).show()
@@ -340,14 +148,12 @@ class Node():
     def __init__(self,
         layer: int,
         head: int,
-        position: int, 
-        #important: bool = False,
+        position: int
     ):
         self.layer = layer
         self.head = head
         self.position = position
-        self.hook_name = get_hook_tuple(self.layer, self.head)
-        #self.important = important
+        #self.hook_name = get_hook_tuple(self.layer, self.head)[0]
         self.children = []
 
     def __repr__(self):
@@ -356,8 +162,6 @@ class Node():
 class HypothesisTree():
     def __init__(self, model: EasyTransformer, metric: Callable, dataset, orig_data, new_data, threshold: int):
         self.model = model
-        #self.node_stack = []
-        #self.current_node = self.node_stack[-1]
         self.possible_positions = [ioi_dataset.word_idx['end']] # TODO: deal with positions
         self.node_stack = OrderedDict()
         self.populate_node_stack()
@@ -368,6 +172,7 @@ class HypothesisTree():
         self.new_data = new_data
         self.threshold = threshold
         self.default_metric = self.metric(model, dataset)
+        self.important_nodes = []
 
     def populate_node_stack(self):
         for layer in range(self.model.cfg.n_layers):
@@ -375,32 +180,46 @@ class HypothesisTree():
                 for pos in self.possible_positions:
                     node = Node(layer, head, pos)
                     self.node_stack[(layer, head, pos)] = node
-                    #self.importance_dict[node] = False
-                    #self.node_stack.append(node)
         layer = self.model.cfg.n_layers
         pos = self.possible_positions[-1] # assume the last position specified is the one that we care about in the residual stream
         resid_post = Node(layer, None, pos) 
-        resid_post.hook_name = f'blocks.{layer-1}.hook_resid_post'
+        #resid_post.hook_name = f'blocks.{layer-1}.hook_resid_post'
         self.node_stack[(layer, None, pos)] = resid_post # this represents blocks.{last}.hook_resid_post
-        #self.node_stack.append(resid_post) 
 
     def eval(self):
         """Process current_node, then move to next current_node"""
 
         _, node = self.node_stack.popitem()
+        self.important_nodes.append(node)
+
+        if node.layer == self.model.cfg.n_layers:
+            receiver_hooks = [
+                (f"blocks.{node.layer-1}.hook_resid_post", None)
+            ]
+        elif node.head is not None:
+            receiver_hooks = [
+                (f"blocks.{node.layer}.attn.hook_q", node.head),
+                (f"blocks.{node.layer}.attn.hook_k", node.head),
+                (f"blocks.{node.layer}.attn.hook_v", node.head)
+            ]
+        else:
+            receiver_hooks = [
+                (f"blocks.{node.layer}.hook_mlp_out", None)
+            ]
 
         # do path patching on all nodes before node
         attn_results, mlp_results = path_patching_up_to(
             model=model, 
+            layer=node.layer,
             metric=self.metric,
             dataset=self.dataset,
-            layer=node.layer,
             orig_data=self.orig_data, 
             new_data=self.new_data, 
-            receiver_hooks=[(node.hook_name, node.head)],
+            receiver_hooks=receiver_hooks,
             position=node.position
         ) 
 
+        # convert to percentage
         attn_results -= self.default_metric
         attn_results /= self.default_metric
         mlp_results -= self.default_metric
@@ -417,8 +236,6 @@ class HypothesisTree():
             if abs(mlp_results[layer]) > self.threshold:
                 self.node_stack[(layer, None, node.position)].children.append(node)
 
-        # iterate to next node
-    
         # update self.current_node
         while len(self.node_stack) > 0 and len(self.node_stack[next(reversed(self.node_stack))].children) == 0:
             self.node_stack.popitem()
@@ -437,52 +254,6 @@ h = HypothesisTree(
     dataset=ioi_dataset, 
     orig_data=ioi_dataset.toks.long(), 
     new_data=abc_dataset.toks.long(), 
-    threshold=0.2)
+    threshold=0.15)
 
 # %%
-
-base_hypothesis = HypothesisTree()
-next_hypotheses = base_hypothesis.expand(1)
-good_hypos = []
-for next_hypo in next_hypotheses:
-    score = next_hypo.eval() # path patching
-    if score > threshold:
-        good_hypos.append(next_hypo)
-base_hypothesis = HypothesisTree(good_hypos)
-
-# %%
-
-# start at blocks.11.hook_resid_post
-# results = path_patching_up_to(11)
-# for h
-threshold = 0.2
-
-receiver_hooks = [('blocks.11.hook_resid_post', None)]
-
-attn_results, mlp_results = path_patching_up_to(
-    model, 
-    layer=12, 
-    metric=logit_diff_io_s,
-    dataset=ioi_dataset,
-    orig_data=ioi_dataset.toks.long(),
-    new_data=abc_dataset.toks.long(),
-    receiver_hooks=receiver_hooks,
-    positions=[ioi_dataset.word_idx['end']])
-
-for layer in range(12):
-    for head in range(12):
-        if attn_results[layer, head] > threshold:
-            receiver_hooks = [
-                (f'blocks.{layer}.attn.hook_q', head),
-                (f'blocks.{layer}.attn.hook_k', head),
-                (f'blocks.{layer}.attn.hook_v', head)]
-            new_attn_results, new_mlp_results = path_patching_up_to(
-                model,
-                layer=layer,
-                metric=logit_diff_io_s,
-                dataset=ioi_dataset,
-                orig_data=ioi_dataset.toks.long(),
-                new_data=abc_dataset.toks.long(),
-                receiver_hooks=receiver_hooks,
-                positions=[ioi_dataset.word_idx['end']]
-            )
