c97bcf9964473c9d3a389c25154c2ec840094c4b
ArthurConmy <arthurconmy@gmail.com>
2022-11-18 15:14:34 -0800

    new acks

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="easy_transformer",
     version="0.1.0",
-    packages=["easy_transformer"],
+    packages=find_packages(),
     license="LICENSE",
     description="An implementation of transformers tailored for mechanistic interpretability.",
     long_description=open("README.md").read(),
