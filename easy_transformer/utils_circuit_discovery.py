0f3e61253d8292652d7ac27a4b60775112f60865
ArthurConmy <arthurconmy@gmail.com>
2022-11-19 18:34:49 -0800

    compare to old

--- easy_transformer/utils_circuit_discovery.py
+++ easy_transformer/utils_circuit_discovery.py
@@ -41,6 +41,133 @@ def patch_positions(z, source_act, hook, positions):
         return z
 
 
+def path_patching_old(
+    model: EasyTransformer,
+    orig_data,
+    new_data,
+    senders: List[Tuple],
+    receiver_hooks: List[Tuple],
+    max_layer: Union[int, None] = None,
+    position: int = 0,
+    return_hooks: bool = False,
+    freeze_mlps: bool = True,
+    orig_cache=None,
+    new_cache=None,
+    prepend_bos=False,  # we did IOI with prepend_bos = False, but in general we think True is less sketchy. Currently EasyTransformer sometimes does one and sometimes does the other : (
+):
+    """MLPs are by default considered as just another component and so are
+    by default frozen when collecting acts on receivers.
+    orig_data: string, torch.Tensor, or list of strings - any format that can be passed to the model directly
+    new_data: same as orig_data
+    senders: list of tuples (layer, head) for attention heads and (layer, None) for mlps
+    receiver_hooks: list of tuples (hook_name, head) for attn heads and (hook_name, None) for mlps
+    max_layer: layers beyond max_layer are not frozen when collecting receiver activations
+    positions: default None and patch at all positions, or a tensor specifying the positions at which to patch
+    NOTE: This relies on a change to the cache_some() function in EasyTransformer/hook_points.py [we .clone() activations, unlike in neelnanda-io/EasyTransformer]
+    """
+    if max_layer is None:
+        max_layer = model.cfg.n_layers
+    assert max_layer <= model.cfg.n_layers
+
+    if orig_cache is None:
+        # save activations from orig
+        orig_cache = {}
+        model.reset_hooks()
+        model.cache_all(orig_cache)
+        _ = model(orig_data, prepend_bos=False)
+
+    # process senders
+    sender_hooks = []
+    for layer, head in senders:
+        if head is not None:  # (layer, head) for attention heads
+            sender_hooks.append((f"blocks.{layer}.attn.hook_result", head))
+        else:  # (layer, None) for mlps
+            sender_hooks.append((f"blocks.{layer}.hook_mlp_out", None))
+    sender_hook_names = [x[0] for x in sender_hooks]
+
+    if new_cache is None:
+        # save activations from new for senders
+        model.reset_hooks()
+        new_cache = {}
+        model.cache_some(new_cache, lambda x: x in sender_hook_names)
+        _ = model(new_data, prepend_bos=False)
+    else:
+        assert all(
+            [x in new_cache for x in sender_hook_names]
+        ), f"Difference between new_cache and senders: {set(sender_hook_names) - set(new_cache.keys())}"
+
+    # set up receiver cache
+    model.reset_hooks()
+    receiver_hook_names = [x[0] for x in receiver_hooks]
+    receiver_cache = {}
+    model.cache_some(receiver_cache, lambda x: x in receiver_hook_names)
+
+    # configure hooks for freezing activations
+    for layer in range(max_layer):
+        # heads
+        for head in range(model.cfg.n_heads):
+            # if (layer, head) in senders:
+            #     continue
+            for hook_template in [
+                "blocks.{}.attn.hook_q",
+                "blocks.{}.attn.hook_k",
+                "blocks.{}.attn.hook_v",
+            ]:
+                hook_name = hook_template.format(layer)
+                hook = get_act_hook(
+                    patch_all,
+                    alt_act=orig_cache[hook_name],
+                    idx=head,
+                    dim=2,
+                )
+                model.add_hook(hook_name, hook)
+        # mlp
+        if freeze_mlps:
+            hook_name = f"blocks.{layer}.hook_mlp_out"
+            hook = get_act_hook(
+                patch_all,
+                alt_act=orig_cache[hook_name],
+                idx=None,
+                dim=None,
+            )
+            model.add_hook(hook_name, hook)
+
+    # for senders, add new hook to patching in new acts
+    for hook_name, head in sender_hooks:
+        # assert not torch.allclose(orig_cache[hook_name], new_cache[hook_name]), (hook_name, head)
+        hook = get_act_hook(
+            partial(patch_positions, positions=[position]),
+            alt_act=new_cache[hook_name],
+            idx=head,
+            dim=2 if head is not None else None,
+        )
+        model.add_hook(hook_name, hook)
+
+    # forward pass on orig, where patch in new acts for senders and orig acts for the rest
+    # and save activations on receivers
+    _ = model(orig_data, prepend_bos=False)
+    model.reset_hooks()
+
+    # add hooks for final forward pass on orig, where we patch in hybrid acts for receivers
+    hooks = []
+    for hook_name, head in receiver_hooks:
+        # assert not torch.allclose(orig_cache[hook_name], receiver_cache[hook_name])
+        hook = get_act_hook(
+            partial(patch_positions, positions=[position]),
+            alt_act=receiver_cache[hook_name],
+            idx=head,
+            dim=2 if head is not None else None,
+        )
+        hooks.append((hook_name, hook))
+
+    if return_hooks:
+        return hooks
+    else:
+        for hook_name, hook in hooks:
+            model.add_hook(hook_name, hook)
+        return model
+
+
 def path_patching(
     model: EasyTransformer,
     orig_data,
