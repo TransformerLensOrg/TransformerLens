72b9be00c12b5be37baaa16b57e5df5e2aff3aad
derpyplops <jonathan.n@u.nus.edu>
2023-01-10 12:32:15 -0800

    Implement permanent hooks (#117)
    * implement perma hook
    
    * types
    
    
    missed a type
    
    * slight refactor
    
    * Tests for add_perma_hook
    
    * typo
    
    * revert Union (|) syntax
    
    * made tests pass
    
    * add another test
    
    * renaming
    
    * more renaming and make more pythonic
    
    * Make more pythonic
    
    * remove redundant test function invocation
    
    * fix hook logic
    
    revert .gitconfig
    
    * Fixed type typo
    
    Co-authored-by: Neel Nanda <neelnanda27@gmail.com>

--- transformer_lens/hook_points.py
+++ transformer_lens/hook_points.py
@@ -1,10 +1,19 @@
 # Import stuff
 import logging
-from typing import Callable, Union, Optional, Sequence
+from typing import Callable, Union, Optional, Sequence, Dict, List, Tuple
 import torch
 import torch.nn as nn
+import torch.utils.hooks as hooks
 
 from functools import *
+
+from dataclasses import dataclass
+
+@dataclass
+class LensHandle:
+    hook: hooks.RemovableHandle
+    is_permanent: bool = False
+    
 
 # %%
 # Define type aliases
@@ -18,45 +27,54 @@ NamesFilter = Optional[Union[Callable[[str], bool], Sequence[str]]]
 class HookPoint(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fwd_hooks = []
-        self.bwd_hooks = []
+        self.fwd_hooks: List[LensHandle] = []
+        self.bwd_hooks: List[LensHandle] = []
         self.ctx = {}
 
         # A variable giving the hook's name (from the perspective of the root
         # module) - this is set by the root module at setup.
         self.name = None
 
-    def add_hook(self, hook, dir="fwd"):
+    def add_perma_hook(self, hook, dir="fwd") -> None:
+        self.add_hook(hook, dir=dir, is_permanent=True)
+
+    def add_hook(self, hook, dir="fwd", is_permanent=False) -> None:
         # Hook format is fn(activation, hook_name)
         # Change it into PyTorch hook format (this includes input and output,
         # which are the same for a HookPoint)
-
         if dir == "fwd":
 
             def full_hook(module, module_input, module_output):
                 return hook(module_output, hook=self)
 
             handle = self.register_forward_hook(full_hook)
+            handle = LensHandle(handle, is_permanent)
             self.fwd_hooks.append(handle)
         elif dir == "bwd":
             # For a backwards hook, module_output is a tuple of (grad,) - I don't know why.
             def full_hook(module, module_input, module_output):
                 return hook(module_output[0], hook=self)
-
             handle = self.register_full_backward_hook(full_hook)
+            handle = LensHandle(handle, is_permanent)
             self.bwd_hooks.append(handle)
         else:
             raise ValueError(f"Invalid direction {dir}")
 
-    def remove_hooks(self, dir="fwd"):
-        if (dir == "fwd") or (dir == "both"):
-            for hook in self.fwd_hooks:
-                hook.remove()
-            self.fwd_hooks = []
-        if (dir == "bwd") or (dir == "both"):
-            for hook in self.bwd_hooks:
-                hook.remove()
-            self.bwd_hooks = []
+    def remove_hooks(self, dir="fwd", including_permanent=False) -> None:
+
+        def _remove_hooks(handles: List[LensHandle]) -> None:
+            output_handles = []
+            for handle in handles:
+                if not handle.is_permanent or including_permanent:
+                    handle.hook.remove()
+                else:
+                    output_handles.append(handle)
+            return output_handles
+
+        if dir == "fwd" or dir == "both":
+            self.fwd_hooks = _remove_hooks(self.fwd_hooks)
+        if dir == "bwd" or dir == "both":
+            self.bwd_hooks = _remove_hooks(self.bwd_hooks)
         if dir not in ["fwd", "bwd", "both"]:
             raise ValueError(f"Invalid direction {dir}")
 
@@ -96,7 +114,7 @@ class HookedRootModule(nn.Module):
         # Add a parameter to each module giving its name
         # Build a dictionary mapping a module name to the module
         self.mod_dict = {}
-        self.hook_dict = {}
+        self.hook_dict: Dict[str, HookPoint] = {}
         for name, module in self.named_modules():
             module.name = name
             self.mod_dict[name] = module
@@ -106,34 +124,37 @@ class HookedRootModule(nn.Module):
     def hook_points(self):
         return self.hook_dict.values()
 
-    def remove_all_hook_fns(self, direction="both"):
+    def remove_all_hook_fns(self, direction="both", including_permanent=False):
         for hp in self.hook_points():
-            hp.remove_hooks(direction)
+            hp.remove_hooks(direction, including_permanent=including_permanent)
 
     def clear_contexts(self):
         for hp in self.hook_points():
             hp.clear_context()
 
-    def reset_hooks(self, clear_contexts=True, direction="both"):
+    def reset_hooks(self, clear_contexts=True, direction="both", including_permanent=False):
         if clear_contexts:
             self.clear_contexts()
-        self.remove_all_hook_fns(direction)
+        self.remove_all_hook_fns(direction, including_permanent)
         self.is_caching = False
 
-    def add_hook(self, name, hook, dir="fwd"):
+    def add_hook(self, name, hook, dir="fwd", is_permanent=False) -> None:
         if type(name) == str:
-            self.mod_dict[name].add_hook(hook, dir=dir)
+            self.mod_dict[name].add_hook(hook, dir=dir, is_permanent=is_permanent)
         else:
             # Otherwise, name is a Boolean function on names
             for hook_name, hp in self.hook_dict.items():
                 if name(hook_name):
-                    hp.add_hook(hook, dir=dir)
+                    hp.add_hook(hook, dir=dir, is_permanent=is_permanent)
+
+    def add_perma_hook(self, name, hook, dir="fwd") -> None:
+        self.add_hook(name, hook, dir=dir, is_permanent=True)
 
     def run_with_hooks(
         self,
         *model_args,
-        fwd_hooks=[],
-        bwd_hooks=[],
+        fwd_hooks: List[Tuple[Union[str, Callable], Callable]] = [],
+        bwd_hooks: List[Tuple[Union[str, Callable], Callable]] = [],
         reset_hooks_end=True,
         clear_contexts=False,
         **model_kwargs,
@@ -171,7 +192,7 @@ class HookedRootModule(nn.Module):
                 logging.warning(
                     "WARNING: Hooks were reset at the end of run_with_hooks while backward hooks were set. This removes the backward hooks before a backward pass can occur"
                 )
-            self.reset_hooks(clear_contexts)
+            self.reset_hooks(clear_contexts, including_permanent=False)
         return out
 
     def add_caching_hooks(
@@ -258,7 +279,7 @@ class HookedRootModule(nn.Module):
             model_out.backward()
 
         if reset_hooks_end:
-            self.reset_hooks(clear_contexts)
+            self.reset_hooks(clear_contexts, including_permanent=False)
         return model_out, cache_dict
 
     def cache_all(self, cache, incl_bwd=False, device=None, remove_batch_dim=False):
---
+++ transformer_lens/tests/test_hooks.py
@@ -0,0 +1,50 @@
+from typeguard.importhook import install_import_hook
+
+install_import_hook("easy_transformer")
+
+from easy_transformer import EasyTransformer
+from torchtyping import TensorType as TT, patch_typeguard
+
+patch_typeguard()
+
+MODEL = "gpt2"
+
+prompt = "Hello World!"
+model = EasyTransformer.from_pretrained(MODEL)
+embed = lambda name: name == "hook_embed"
+
+class Counter:
+    def __init__(self):
+        self.count = 0
+
+    def inc(self, *args, **kwargs):
+        self.count += 1
+
+def test_hook_attaches_normally():
+    c = Counter()
+    _ = model.run_with_hooks(prompt, fwd_hooks=[(embed, c.inc)])
+    assert all([len(hp.fwd_hooks) == 0 for _, hp in model.hook_dict.items()])
+    assert c.count == 1
+    model.remove_all_hook_fns(including_permanent=True)
+
+def test_perma_hook_attaches_normally():
+    c = Counter()
+    model.add_perma_hook(embed, c.inc)
+    assert len(model.hook_dict['hook_embed'].fwd_hooks) == 1
+    model.run_with_hooks(prompt, fwd_hooks=[])
+    assert len(model.hook_dict['hook_embed'].fwd_hooks) == 1
+    assert c.count == 1
+    model.remove_all_hook_fns(including_permanent=True)
+
+def test_remove_hook():
+    c = Counter()
+    model.add_perma_hook(embed, c.inc)
+    assert len(model.hook_dict['hook_embed'].fwd_hooks) == 1 # 1 after adding
+    model.remove_all_hook_fns()
+    assert len(model.hook_dict['hook_embed'].fwd_hooks) == 1 # permanent not removed without flag
+    model.remove_all_hook_fns(including_permanent=True)
+    assert len(model.hook_dict['hook_embed'].fwd_hooks) == 0 # removed now
+    model.run_with_hooks(prompt, fwd_hooks=[])
+    assert c.count == 0
+    model.remove_all_hook_fns(including_permanent=True)
+
