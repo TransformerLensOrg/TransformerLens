e554e20c997f0124e88574f7d2b4ebfa962130d4
dkamm <davidfkamm@gmail.com>
2023-01-17 16:49:05 +0100

    Apply Neel fix

--- transformer_lens/torchtyping_helper.py
+++ transformer_lens/torchtyping_helper.py
@@ -1,6 +1,4 @@
-from enum import Enum
-
-class T(Enum):
+class T:
     """Helper class to get mypy to work with TorchTyping and solidify naming conventions as a byproduct.
     
     Examples: 
@@ -9,39 +7,35 @@ class T(Enum):
     - `TT[T.d_vocab + T.n_ctx, T.d_model]`
     """
 
-    batch = "batch"
-    pos = "pos"
-    head_index = "head_index"
-    length = "length"
-    rotary_dim = "rotary_dim"
-    new_tokens = "new_tokens"
-    batch_and_pos_dims = "batch_and_pos_dims"
-    layers_accumulated_over = "layers_accumulated_over"
-    layers_covered = "layers_covered"
-    past_kv_pos_offset = "past_kv_pos_offset"
-    num_components = "num_components"
-    num_neurons = "num_neurons"
-    pos_so_far = "pos_so_far"
-    n_ctx = "n_ctx"
-    n_heads = "n_heads"
-    n_layers = "n_layers"
-    d_vocab = "d_vocab"
-    d_vocab_out = "d_vocab_out"
-    d_head = "d_head"
-    d_mlp = "d_mlp"
-    d_model = "d_model"
+    batch: str = "batch"
+    pos: str = "pos"
+    head_index: str = "head_index"
+    length: str = "length"
+    rotary_dim: str = "rotary_dim"
+    new_tokens: str = "new_tokens"
+    batch_and_pos_dims: str = "batch_and_pos_dims"
+    layers_accumulated_over: str = "layers_accumulated_over"
+    layers_covered: str = "layers_covered"
+    past_kv_pos_offset: str = "past_kv_pos_offset"
+    num_components: str = "num_components"
+    num_neurons: str = "num_neurons"
+    pos_so_far: str = "pos_so_far"
+    n_ctx: str = "n_ctx"
+    n_heads: str = "n_heads"
+    n_layers: str = "n_layers"
+    d_vocab: str = "d_vocab"
+    d_vocab_out: str = "d_vocab_out"
+    d_head: str = "d_head"
+    d_mlp: str = "d_mlp"
+    d_model: str = "d_model"
 
-    ldim = "ldim"
-    rdim = "rdim"
-    new_rdim = "new_rdim"
-    mdim = "mdim"
-    leading_dims = "leading_dims"
-    leading_dims_left = "leading_dims_left"
-    leading_dims_right = "leading_dims_right"
+    ldim: str = "ldim"
+    rdim: str = "rdim"
+    new_rdim: str = "new_rdim"
+    mdim: str = "mdim"
+    leading_dims: str = "leading_dims"
+    leading_dims_left: str = "leading_dims_left"
+    leading_dims_right: str = "leading_dims_right"
 
-    a = "a"
-    b = "b"
-
-    def __add__(self, other: "T") -> str:
-        """Hack to let us write type expressions like `T.a + T.b`"""
-        return f"{self.value} + {other.value}"
+    a: str = "a"
+    b: str = "b"
