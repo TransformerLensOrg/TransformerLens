3758868784c43e2d5a500bf0331b8fe6ac697ddf
ckkissane <ckkissane@gmail.com>
2023-04-02 14:09:21 -0400

    squeeze 0th dim in vector FactoredMatrix multiply

--- tests/unit/factored_matrix/test_multiply_by_vector.py
+++ tests/unit/factored_matrix/test_multiply_by_vector.py
@@ -25,18 +25,6 @@ def test_left_matmul_by_vector_leading_dim():
     assert_close(fm @ vector, (a @ b) @ vector)
 
 
-@pytest.mark.skip(
-    """
-    This test fails with
-    AssertionError: The values for attribute 'shape' do not match: torch.Size([1, 4]) != torch.Size([4]).
-
-    I think this is a bug in __rmatmul__:
-    `return ((other.unsqueeze(-2) @ self.A) @ self.B).squeeze(-1)` is squeezing on the wrong dimension.
-
-    We could fix this by changing the argument to squeeze to `-2`. However there would be a small risk of
-    breaking consumers that assume the existing behaviour?
-    """
-)
 def test_right_matmul_by_vector():
     a = torch.rand(2, 3)
     b = torch.rand(3, 4)
@@ -47,16 +35,6 @@ def test_right_matmul_by_vector():
     assert_close(vector @ fm, vector @ (a @ b))
 
 
-@pytest.mark.skip(
-    """
-    See comment for [test_right_matmul_by_vector].
-
-    This time, error is
-    AssertionError: The values for attribute 'shape' do not match: torch.Size([6, 1, 4]) != torch.Size([6, 4]).
-
-    Fix would be the same.
-    """
-)
 def test_right_matmul_by_vector_leading_dim():
     a = torch.rand(6, 2, 3)
     b = torch.rand(6, 3, 4)
