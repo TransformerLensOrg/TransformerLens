eab78c115e82af5bc44fce1bb01c086813b05627
koayon <58336584+koayon@users.noreply.github.com>
2023-05-10 17:35:00 +0100

    Add logo and icon to sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,27 +6,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'TransformerLens'
-copyright = '2023, Neel Nanda'
-author = 'Neel Nanda'
-release = '0.0.0'
+project = "TransformerLens"
+copyright = "2023, Neel Nanda"
+author = "Neel Nanda"
+release = "0.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc',
-              'sphinxcontrib.napoleon',
-              'myst_parser',
-              "sphinx.ext.githubpages"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "myst_parser",
+    "sphinx.ext.githubpages",
+]
 
 source_suffix = {
     ".rst": "restructuredtext",
     ".md": "markdown",
 }
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -34,4 +35,8 @@ exclude_patterns = []
 
 html_theme = "furo"
 html_title = "TransformerLens Documentation"
-html_static_path = ['_static']
+html_static_path = ["_static"]
+
+html_logo = "_static/transformer_lens_logo.png"
+
+html_favicon = "favicon.ico"
