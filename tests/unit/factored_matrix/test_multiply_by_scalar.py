0f71dc4744f8a2f8deaaf3e32ea96999f347b8d0
matthiasdellago <matthias.dellago@proton.me>
2023-07-23 13:19:28 +0200

    Tests for errors when multiplying by non-scalar

--- tests/unit/factored_matrix/test_multiply_by_scalar.py
+++ tests/unit/factored_matrix/test_multiply_by_scalar.py
@@ -4,29 +4,38 @@ from torch.testing import assert_close
 
 from transformer_lens import FactoredMatrix
 
+# This test function is parametrized with different types of scalars, including non-scalar tensors and arrays, to check that the correct errors are raised.
+# Considers cases with and without leading dimensions as well as left and right multiplication.
 @pytest.mark.parametrize(
-    "scalar", 
+    "scalar, error_expected", 
     [
-        torch.rand(1), 
-        float(torch.rand(1).item()), 
-        int(torch.randint(1, 10, (1,)).item())
+        # Test cases with different types of scalar values.
+        (torch.rand(1), None),  # 1-element Tensor. No error expected.
+        (float(torch.rand(1).item()), None),  # float. No error expected.
+        (int(torch.randint(1, 10, (1,)).item()), None),  # int. No error expected.
+
+        # Test cases with non-scalar values that are expected to raise errors.
+        (torch.rand(2, 2), AssertionError),  # Non-scalar Tensor. AssertionError expected.
+        (torch.rand(2), AssertionError),  # Non-scalar Tensor. AssertionError expected.
+
     ]
 )
 @pytest.mark.parametrize(
     "leading_dim", 
     [
-        False, 
+        False,
         True
     ]
 )
 @pytest.mark.parametrize(
     "multiply_from_left", 
     [
-        False, 
+        False,
         True
     ]
 )
-def test_multiply_by_scalar(scalar, leading_dim, multiply_from_left):
+def test_multiply(scalar, leading_dim, multiply_from_left, error_expected):
+    # Prepare a FactoredMatrix, with or without leading dimensions
     if leading_dim:
         a = torch.rand(6, 2, 3)
         b = torch.rand(6, 3, 4)
@@ -36,7 +45,17 @@ def test_multiply_by_scalar(scalar, leading_dim, multiply_from_left):
 
     fm = FactoredMatrix(a, b)
 
-    if multiply_from_left:
-        assert_close((fm * scalar).AB, (a @ b) * scalar)
+    if error_expected:
+        # If an error is expected, check that the correct exception is raised.
+        with pytest.raises(error_expected):
+            if multiply_from_left:
+                _ = fm * scalar
+            else:
+                _ = scalar * fm
     else:
-        assert_close((scalar * fm).AB, (a @ b) * scalar)
+        # If no error is expected, check that the multiplication results in the correct value.
+        # Use FactoredMatrix.AB to calculate the product of the two factor matrices before comparing with the expected value.
+        if multiply_from_left:
+            assert_close((fm * scalar).AB, (a @ b) * scalar)
+        else:
+            assert_close((scalar * fm).AB, (a @ b) * scalar)
