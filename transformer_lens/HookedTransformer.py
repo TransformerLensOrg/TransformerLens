36c275d705825974fc321c0fa6d427e2f3490141
Sohee Yang <soheeyang.cs@gmail.com>
2023-09-09 19:34:45 +0000

    fix(from_pretrained_no_processing): add missing params

--- transformer_lens/HookedTransformer.py
+++ transformer_lens/HookedTransformer.py
@@ -1122,6 +1122,8 @@ class HookedTransformer(HookedRootModule):
         center_unembed=False,
         refactor_factored_attn_matrices=False,
         fold_value_biases=False,
+        default_prepend_bos=True,
+        default_padding_side="right",
         **from_pretrained_kwargs,
     ):
         """Wrapper for from_pretrained with all boolean flags related to simplifying the model set to False. Refer to
@@ -1133,6 +1135,8 @@ class HookedTransformer(HookedRootModule):
             center_unembed=center_unembed,
             fold_value_biases=fold_value_biases,
             refactor_factored_attn_matrices=refactor_factored_attn_matrices,
+            default_prepend_bos=default_prepend_bos,
+            default_padding_side=default_padding_side,
             **from_pretrained_kwargs,
         )
 
