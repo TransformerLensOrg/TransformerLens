7148e9dcd81e2f36ea6c0c841265ce04f1d815d1
Alan Cooney <41682961+alan-cooney@users.noreply.github.com>
2023-10-12 14:48:14 +0800

    Simplify docs generation process

--- transformer_lens/make_docs.py
+++ transformer_lens/make_docs.py
@@ -1,69 +1,87 @@
-# %%
-from functools import lru_cache
-
-from easy_transformer import loading
-
-# %%
-# cfg = (loading.get_pretrained_model_config("solu-1l"))
-# print(cfg)
-# %%
 """ 
-Structure:
-d_model, d_mlp, d_head, d_vocab, act_fn, n_heads, n_layers, n_ctx, n_params, 
-Make an architecture table separately probs
-tokenizer_name, training_data, has checkpoints
-act_fn includes attn_only
-architecture
-Architecture should list weird shit to be aware of.
+Generate a markdown table summarizing properties of pretrained models.
+
+This script extracts various properties of pretrained models from the 
+`easy_transformer` library, such as the number of parameters, layers, and heads, 
+among others, and generates a markdown table. This table is saved to the 
+docs directory.
 """
+from functools import lru_cache
+from pathlib import Path
+
 import pandas as pd
 
+from transformer_lens import loading
 
-# df = pd.DataFrame(np.random.randn(2, 2))
-# print(df.to_markdown(open("test.md", "w")))
-# %%
+
 @lru_cache(maxsize=None)
 def get_config(model_name):
+    """Retrieve the configuration of a pretrained model.
+
+    Args:
+        model_name (str): Name of the pretrained model.
+
+    Returns:
+        dict: Configuration of the pretrained model.
+    """
     return loading.get_pretrained_model_config(model_name)
 
 
 def get_property(name, model_name):
+    """Retrieve a specific property of a pretrained model.
+
+    Args:
+        name (str): Name of the property to retrieve.
+        model_name (str): Name of the pretrained model.
+
+    Returns:
+        str: Value of the specified property.
+    """
     cfg = get_config(model_name)
+
     if name == "act_fn":
         if cfg.attn_only:
             return "attn_only"
-        elif cfg.act_fn == "gelu_new":
+        if cfg.act_fn == "gelu_new":
             return "gelu"
-        elif cfg.act_fn == "gelu_fast":
+        if cfg.act_fn == "gelu_fast":
             return "gelu"
-        elif cfg.act_fn == "solu_ln":
+        if cfg.act_fn == "solu_ln":
             return "solu"
-        else:
-            return cfg.act_fn
+        return cfg.act_fn
     if name == "n_params":
         n_params = cfg.n_params
         if n_params < 1e4:
             return f"{n_params/1e3:.1f}K"
-        elif n_params < 1e6:
+        if n_params < 1e6:
             return f"{round(n_params/1e3)}K"
-        elif n_params < 1e7:
+        if n_params < 1e7:
             return f"{n_params/1e6:.1f}M"
-        elif n_params < 1e9:
+        if n_params < 1e9:
             return f"{round(n_params/1e6)}M"
-        elif n_params < 1e10:
+        if n_params < 1e10:
             return f"{n_params/1e9:.1f}B"
-        elif n_params < 1e12:
+        if n_params < 1e12:
             return f"{round(n_params/1e9)}B"
-        else:
-            raise ValueError(f"Passed in {n_params} above 1T?")
-    else:
-        return cfg.to_dict()[name]
+        raise ValueError(f"Passed in {n_params} above 1T?")
+    return cfg.to_dict()[name]
 
 
-if __name__ == "__main__":
-    column_names = "n_params, n_layers, d_model, n_heads, act_fn, n_ctx, d_vocab, d_head, d_mlp".split(
-        ", "
-    )
+def main():
+    """Generate a markdown table summarizing properties of pretrained models."""
+
+    # Create the table
+    column_names = [
+        "n_params",
+        "n_layers",
+        "d_model",
+        "n_heads",
+        "act_fn",
+        "n_ctx",
+        "d_vocab",
+        "d_head",
+        "d_mlp",
+    ]
     df = pd.DataFrame(
         {
             name: [
@@ -74,4 +92,17 @@ if __name__ == "__main__":
         },
         index=loading.DEFAULT_MODEL_ALIASES,
     )
-    df.to_markdown(open("model_properties_table.md", "w"))
+
+    # Convert to markdown (with a title)
+    markdown_string = df.to_markdown()
+    markdown_string = "# Model Properties Table\n\n" + markdown_string
+
+    # Save to the docs directory
+    current_dir = Path(__file__).parent
+    file_path = current_dir / "../docs/source/model_properties_table.md"
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(markdown_string)
+
+
+if __name__ == "__main__":
+    main()
