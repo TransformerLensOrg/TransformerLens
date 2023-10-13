892c9c1107b152e619a57e08e2619f9fe88547af
Arthur Conmy <arthurconmy@gmail.com>
2023-10-13 01:23:33 +0000

    Fix test

--- transformer_lens/HookedTransformer.py
+++ transformer_lens/HookedTransformer.py
@@ -719,6 +719,7 @@ class HookedTransformer(HookedRootModule):
     def to_string(
         self,
         tokens: Union[
+            List[int],
             Int[torch.Tensor, ""],
             Int[torch.Tensor, "batch pos"],
             Int[torch.Tensor, "pos"],
