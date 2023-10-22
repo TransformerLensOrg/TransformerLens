56077135524ec1c37f690f40d1843e906ecc16e9
Bryce Meyer <bryce13950@gmail.com>
2023-10-22 23:43:23 +0200

    skipped plain text comparison

--- demos/conftest.py
+++ demos/conftest.py
@@ -3,6 +3,7 @@ def pytest_collectstart(collector):
     if collector.fspath and collector.fspath.ext == ".ipynb":
         collector.skip_compare += (
             "text/html",
+            "text/plain",
             "application/javascript",
             "application/vnd.plotly.v1+json",  # Plotly
         )
