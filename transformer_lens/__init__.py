af99428ce64764b9c2d40098081604b27bb48595
Bryce Meyer <bryce13950@gmail.com>
2023-12-11 19:20:03 +0100

    added import again

--- transformer_lens/__init__.py
+++ transformer_lens/__init__.py
@@ -5,6 +5,7 @@ from .past_key_value_caching import (
     HookedTransformerKeyValueCache,
     HookedTransformerKeyValueCacheEntry,
 )
+from . import components
 from .HookedTransformerConfig import HookedTransformerConfig
 from .FactoredMatrix import FactoredMatrix
 from .ActivationCache import ActivationCache
