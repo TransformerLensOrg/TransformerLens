b3beaa90a41f41e06d1f79740d42844827dc7c90
Bryce Meyer <bryce13950@gmail.com>
2024-05-10 22:43:07 +0200

    set coverage as extra pth

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,8 @@ napoleon_custom_sections = [
 
 html_theme = "furo"
 html_title = "TransformerLens Documentation"
-html_static_path = ["_static", "coverage"]
+html_static_path = ["_static"]
+html_extra_path = ["coverage"]
 html_logo = "_static/transformer_lens_logo.png"
 html_favicon = "favicon.ico"
 
