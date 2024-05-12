9cbed8caa339ac4b48111925c8b3d37db1e99e98
somvy <xenon.a@ya.ru>
2024-05-13 02:00:20 +0300

    feat: add weight and config loading for T5

--- transformer_lens/__init__.py
+++ transformer_lens/__init__.py
@@ -15,6 +15,7 @@ from .HookedSAE import HookedSAE
 from .HookedSAETransformer import HookedSAETransformer
 from .SVDInterpreter import SVDInterpreter
 from .HookedEncoder import HookedEncoder
+from .HookedEncoderDecoder import HookedEncoderDecoder
 from . import head_detector
 from . import loading_from_pretrained as loading
 from . import patching
