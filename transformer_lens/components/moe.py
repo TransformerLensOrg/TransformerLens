09e1e9acd87266ec7599d22d3c7748e128a5be9f
Bryce Meyer <bryce13950@gmail.com>
2024-06-14 01:49:45 +0200

    updated dimensions

--- transformer_lens/components/moe.py
+++ transformer_lens/components/moe.py
@@ -56,7 +56,8 @@ class MoE(nn.Module):
         # [batch, pos, d_model] -> [batch, pos, num_experts]
 
         batch_size, sequence_length, hidden_dim = x.shape
-        hidden_states = x.view(-1, hidden_dim)
+        hidden_states = x
+        # hidden_states = x.view(-1, hidden_dim)
         # router_logits: (batch * sequence_length, n_experts)
         router_logits = self.gate(hidden_states)
 
@@ -67,7 +68,7 @@ class MoE(nn.Module):
         routing_weights = routing_weights.to(hidden_states.dtype)
 
         final_hidden_states = torch.zeros(
-            (batch_size * sequence_length, hidden_dim), dtype=hidden_states.dtype, device=hidden_states.device
+            (batch_size, sequence_length, hidden_dim), dtype=hidden_states.dtype, device=hidden_states.device
         )
 
         # One hot encode the selected experts to create an expert mask
@@ -82,12 +83,13 @@ class MoE(nn.Module):
             # Index the correct hidden states and compute the expert hidden state for
             # the current expert. We need to make sure to multiply the output hidden
             # states by `routing_weights` on the corresponding tokens (top-1 and top-2)
-            current_state = hidden_states[None, top_x]
+            print(hidden_states[None, top_x].reshape(-1, hidden_dim).shape)
+            current_state = hidden_states[None, top_x].reshape(-1, hidden_dim)
             current_hidden_states = expert_layer(current_state) * routing_weights[top_x, idx, None]
 
             # However `index_add_` only support torch tensors for indexing so we'll use
             # the `top_x` tensor here.
-            final_hidden_states.index_add_(top_x, current_hidden_states.to(hidden_states.dtype))
+            final_hidden_states.index_add_(0, top_x, current_hidden_states.to(hidden_states.dtype))
         final_hidden_states = final_hidden_states.reshape(batch_size, sequence_length, hidden_dim)
         return final_hidden_states, router_logits
 
