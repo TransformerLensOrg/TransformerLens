8a056e3869ed981390bf7bbfdcf9ea260bacd1e5
Bryce Meyer <bryce13950@gmail.com>
2024-06-14 00:05:02 +0200

    matched hugging face forward

--- transformer_lens/components/moe.py
+++ transformer_lens/components/moe.py
@@ -51,9 +51,10 @@ class MoE(nn.Module):
 
         # choose the top k(=experts_per_token) experts to use
         # both are [batch, pos, experts_per_token]
-        weights = self.hook_expert_weights(F.softmax(gate_logits, dim=-1))
+        weights = self.hook_expert_weights(F.softmax(gate_logits, dim=1, dtype=torch.float))
         weights, expert_indices = torch.topk(weights, self.experts_per_token, dim=-1)
         weights /= weights.sum(dim=-1, keepdim=True)
+        weights = weights.to(gate_logits.dtype)
         expert_indices = self.hook_expert_indices(expert_indices)
 
         results = torch.zeros_like(x)
