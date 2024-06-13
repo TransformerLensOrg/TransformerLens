f76b55a7b0cdcb31aa8b079318533fc334adb552
Bryce Meyer <bryce13950@gmail.com>
2024-06-14 01:05:31 +0200

    synced to transformers implementation

--- transformer_lens/components/moe.py
+++ transformer_lens/components/moe.py
@@ -58,20 +58,41 @@ class MoE(nn.Module):
             x,
             self.W_gate,
         )
+        batch_size, sequence_length, hidden_dim = gate_logits.shape
+        if self.training and self.jitter_noise > 0:
+            gate_logits *= torch.empty_like(gate_logits).uniform_(1.0 - self.jitter_noise, 1.0 + self.jitter_noise)
+        hidden_states = gate_logits.view(-1, hidden_dim)
+        # router_logits: (batch * sequence_length, n_experts)
+        router_logits = self.gate(hidden_states)
 
-        # choose the top k(=experts_per_token) experts to use
-        # both are [batch, pos, experts_per_token]
-        weights, expert_indices = torch.topk(gate_logits, self.experts_per_token, dim=-1)
-        weights = self.hook_expert_weights(F.softmax(weights, dim=-1))
-        expert_indices = self.hook_expert_indices(expert_indices)
+        routing_weights = F.softmax(router_logits, dim=1, dtype=torch.float)
+        routing_weights, selected_experts = torch.topk(routing_weights, self.top_k, dim=-1)
+        routing_weights /= routing_weights.sum(dim=-1, keepdim=True)
+        # we cast back to the input dtype
+        routing_weights = routing_weights.to(hidden_states.dtype)
 
-        results = torch.zeros_like(x)
-        for i, expert_mlp in enumerate(self.experts):
-            mask = (expert_indices == i)
-            if not mask.any():
-                continue
-            batch, pos, expert = torch.where(mask)
-            # accumulate the weighted outputs from the expert
-            results[batch] += weights[batch, pos, expert, None, None] * expert_mlp(x[batch])
+        final_hidden_states = torch.zeros(
+            (batch_size * sequence_length, hidden_dim), dtype=hidden_states.dtype, device=hidden_states.device
+        )
 
-        return results
+        # One hot encode the selected experts to create an expert mask
+        # this will be used to easily index which expert is going to be sollicitated
+        expert_mask = F.one_hot(selected_experts, num_classes=self.cfg.num_experts).permute(2, 1, 0)
+
+        # Loop over all available experts in the model and perform the computation on each expert
+        for expert_idx in range(self.cfg.num_experts):
+            expert_layer = self.experts[expert_idx]
+            idx, top_x = torch.where(expert_mask[expert_idx])
+
+            # Index the correct hidden states and compute the expert hidden state for
+            # the current expert. We need to make sure to multiply the output hidden
+            # states by `routing_weights` on the corresponding tokens (top-1 and top-2)
+            current_state = hidden_states[None, top_x].reshape(-1, hidden_dim)
+            current_hidden_states = expert_layer(current_state) * routing_weights[top_x, idx, None]
+
+            # However `index_add_` only support torch tensors for indexing so we'll use
+            # the `top_x` tensor here.
+            final_hidden_states.index_add_(0, top_x, current_hidden_states.to(hidden_states.dtype))
+        final_hidden_states = final_hidden_states.reshape(batch_size, sequence_length, hidden_dim)
+        return final_hidden_states, router_logits
+
