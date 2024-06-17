788d355cd9cff896685c711793f9b42f826ea3a9
Bryce Meyer <bryce13950@gmail.com>
2024-06-17 17:25:23 +0200

    set device to grouped query attention

--- transformer_lens/components/grouped_query_attention.py
+++ transformer_lens/components/grouped_query_attention.py
@@ -37,6 +37,7 @@ class GroupedQueryAttention(AbstractAttention):
                 self.cfg.d_model,
                 self.cfg.d_head,
                 dtype=cfg.dtype,
+                device=cfg.device,
             )
         )
         self._W_V = nn.Parameter(
@@ -45,13 +46,14 @@ class GroupedQueryAttention(AbstractAttention):
                 self.cfg.d_model,
                 self.cfg.d_head,
                 dtype=cfg.dtype,
+                device=cfg.device,
             )
         )
         self._b_K = nn.Parameter(
-            torch.zeros(cfg.n_key_value_heads, self.cfg.d_head, dtype=cfg.dtype)
+            torch.zeros(cfg.n_key_value_heads, self.cfg.d_head, dtype=cfg.dtype, device=cfg.device)
         )
         self._b_V = nn.Parameter(
-            torch.zeros(cfg.n_key_value_heads, self.cfg.d_head, dtype=cfg.dtype)
+            torch.zeros(cfg.n_key_value_heads, self.cfg.d_head, dtype=cfg.dtype, device=cfg.device)
         )
 
     @property
