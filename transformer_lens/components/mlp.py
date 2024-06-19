160b2ba055660c5a2c2eec21b21c8a0d1c0af87d
Joel Burget <joelburget@gmail.com>
2024-06-19 07:56:49 +0100

    Add jaxtypes for `batch_addmm`.

--- transformer_lens/components/mlp.py
+++ transformer_lens/components/mlp.py
@@ -16,15 +16,31 @@ from transformer_lens.HookedTransformerConfig import HookedTransformerConfig
 from transformer_lens.utils import gelu_fast, gelu_new, solu
 
 
-def addmm(bias, weight, x):
-    """Fused addmm.
+def vanilla_addmm(
+    input: Float[torch.Tensor, "... #o"],  # Must be broadcastable to "m o"
+    mat1: Float[torch.Tensor, "m n"],
+    mat2: Float[torch.Tensor, "n o"],
+) -> Float[torch.Tensor, "m o"]:
+    """Typechecked version of torch.addmm.
+
+    Note that both mat1 and mat2 *must* be 2d matrices.
+    """
+    return torch.addmm(input, mat1, mat2)
+
+
+def batch_addmm(
+    bias: Float[torch.Tensor, "... #d_out"],  # Must be broadcastable to "... d_out"
+    weight: Float[torch.Tensor, "d_in d_out"],
+    x: Float[torch.Tensor, "... d_in"],
+) -> Float[torch.Tensor, "... d_out"]:
+    """Fused add-multiply with support for batch dimensions.
 
     Must match the Huggingface Conv1D implementation exactly.
     https://github.com/huggingface/transformers/blob/9ba9369a2557e53a01378199a9839ec6e82d8bc7/src/transformers/pytorch_utils.py#L102-L106
     """
-    nf = weight.shape[1]
-    size_out = x.size()[:-1] + (nf,)
-    x = torch.addmm(bias, x.view(-1, x.size(-1)), weight)
+    n_output_features = weight.shape[-2]
+    size_out = x.size()[:-1] + (n_output_features,)
+    x = vanilla_addmm(bias, x.view(-1, x.size(-1)), weight)
     x = x.view(size_out)
     return x
 
@@ -78,10 +94,10 @@ class MLP(nn.Module):
         # This is equivalent to (roughly) W_in @ x + b_in. It's important to
         # use a fused addmm to ensure it matches the Huggingface implementation
         # exactly.
-        pre_act = addmm(self.b_in, self.W_in, x)  # [batch, pos, d_mlp]
+        pre_act = batch_addmm(self.b_in, self.W_in, x)  # [batch, pos, d_mlp]
         if self.cfg.act_fn is not None and not self.cfg.act_fn.endswith("_ln"):
             post_act = self.hook_post(self.act_fn(pre_act))  # [batch, pos, d_mlp]
         else:
             mid_act = self.hook_mid(self.act_fn(pre_act))  # [batch, pos, d_mlp]
             post_act = self.hook_post(self.ln(mid_act))
-        return addmm(self.b_out, self.W_out, post_act)
+        return batch_addmm(self.b_out, self.W_out, post_act)
