b26090ad9c700ef98cfce74db7ae8ea8db6e0994
Bryce Meyer <bryce13950@gmail.com>
2024-06-27 23:38:33 +0200

    fixed format

--- transformer_lens/pretrained/weight_conversions/mixtral.py
+++ transformer_lens/pretrained/weight_conversions/mixtral.py
@@ -1,7 +1,6 @@
 import einops
 import torch
-from typing import Dict
-from transformers.models.mixtral import MixtralModel, MixtralDecoderLayer
+from transformers.models.mixtral import MixtralDecoderLayer, MixtralModel
 
 from transformer_lens.HookedTransformerConfig import HookedTransformerConfig
 
@@ -17,12 +16,13 @@ def _convert_expert_weights(state_dict: dict) -> dict:
     state_dict[f"blocks.{l}.mlp.experts.{e}.W_out"] = (
         model.layers[l].block_sparse_moe.experts[e].w2.weight.T
     )
-    state_dict[f"blocks.{l}.mlp.experts.{e}.b_out"] = torch.zeros(
-        cfg.d_model, dtype=cfg.dtype
-    )
+    state_dict[f"blocks.{l}.mlp.experts.{e}.b_out"] = torch.zeros(cfg.d_model, dtype=cfg.dtype)
     return state_dict
 
-def _convert_layer_weights(cfg: HookedTransformerConfig, state_dict: dict, l: int, layer: MixtralDecoderLayer) -> dict:
+
+def _convert_layer_weights(
+    cfg: HookedTransformerConfig, state_dict: dict, l: int, layer: MixtralDecoderLayer
+) -> dict:
     state_dict[f"blocks.{l}.ln1.w"] = layer.input_layernorm.weight
 
     W_Q = layer.self_attn.q_proj.weight
@@ -59,20 +59,19 @@ def _convert_layer_weights(cfg: HookedTransformerConfig, state_dict: dict, l: in
     # w3 -> W_in
     # See https://github.com/mistralai/mistral-inference/blob/8598cf582091a596671be31990448e0620017851/mistral/model.py#L128 for reference
     for e in range(cfg.num_experts):
-        state_dict[f"blocks.{l}.mlp.experts.{e}.W_in"] = (
-            layer.block_sparse_moe.experts[e].w3.weight.T
-        )
-        state_dict[f"blocks.{l}.mlp.experts.{e}.W_gate"] = (
-            layer.block_sparse_moe.experts[e].w1.weight.T
-        )
+        state_dict[f"blocks.{l}.mlp.experts.{e}.W_in"] = layer.block_sparse_moe.experts[
+            e
+        ].w3.weight.T
+        state_dict[f"blocks.{l}.mlp.experts.{e}.W_gate"] = layer.block_sparse_moe.experts[
+            e
+        ].w1.weight.T
         state_dict[f"blocks.{l}.mlp.experts.{e}.b_in"] = torch.zeros(cfg.d_mlp, dtype=cfg.dtype)
-        state_dict[f"blocks.{l}.mlp.experts.{e}.W_out"] = (
-            layer.block_sparse_moe.experts[e].w2.weight.T
-        )
-        state_dict[f"blocks.{l}.mlp.experts.{e}.b_out"] = torch.zeros(
-            cfg.d_model, dtype=cfg.dtype
-        )
+        state_dict[f"blocks.{l}.mlp.experts.{e}.W_out"] = layer.block_sparse_moe.experts[
+            e
+        ].w2.weight.T
+        state_dict[f"blocks.{l}.mlp.experts.{e}.b_out"] = torch.zeros(cfg.d_model, dtype=cfg.dtype)
     return state_dict
+
 
 def convert_mixtral_weights(hf_model: any, cfg: HookedTransformerConfig):
     # The same as Mistral, but with the MLP replaced with MoE
@@ -83,17 +82,14 @@ def convert_mixtral_weights(hf_model: any, cfg: HookedTransformerConfig):
     assert cfg.n_key_value_heads is not None  # keep mypy happy
     assert cfg.d_mlp is not None
     assert cfg.num_experts is not None
-    
+
     model: MixtralModel = hf_model.model
-    
 
     state_dict["embed.W_E"] = model.embed_tokens.weight
 
     for l in range(cfg.n_layers):
         layer = model.layers[l]
         state_dict = _convert_layer_weights(layer)
-        
-
 
     state_dict["ln_final.w"] = model.norm.weight.data
 
