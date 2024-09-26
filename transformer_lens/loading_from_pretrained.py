2f3f1ae4aa01454ccb3f601120584bb42754f350
Bryce Meyer <bryce13950@gmail.com>
2024-09-27 01:31:31 +0200

    configured instruct models

--- transformer_lens/loading_from_pretrained.py
+++ transformer_lens/loading_from_pretrained.py
@@ -948,11 +948,11 @@ def convert_hf_model_config(model_name: str, **kwargs):
         }
     elif "Llama-3.2-3B-Instruct" in official_model_name:
         cfg_dict = {
-            "d_model": 2048,
-            "d_head": 64,
-            "n_heads": 32,
+            "d_model": 3072,
+            "d_head": 128,
+            "n_heads": 24,
             "d_mlp": 8192,
-            "n_layers": 16,
+            "n_layers": 28,
             "n_ctx": 2048,  # capped due to memory issues
             "eps": 1e-5,
             "d_vocab": 128256,
@@ -961,7 +961,7 @@ def convert_hf_model_config(model_name: str, **kwargs):
             "normalization_type": "RMS",
             "positional_embedding_type": "rotary",
             "rotary_adjacent_pairs": False,
-            "rotary_dim": 64,
+            "rotary_dim": 128,
             "final_rms": True,
             "gated_mlp": True,
         }
