58ae081ee4fbddd175956f8510bf7a0bce1e2291
Bryce Meyer <bryce13950@gmail.com>
2024-10-07 23:04:32 +0200

    updated config check

--- transformer_lens/loading_from_pretrained.py
+++ transformer_lens/loading_from_pretrained.py
@@ -755,7 +755,7 @@ def convert_hf_model_config(model_name: str, **kwargs):
             "final_rms": True,
             "gated_mlp": True,
         }
-    elif official_model_name.startswith("CodeLlama-7b"):  # same architecture CodeLlama and Llama-2
+    elif "CodeLlama-7b" in official_model_name:  # same architecture CodeLlama and Llama-2
         cfg_dict = {
             "d_model": 4096,
             "d_head": 4096 // 32,
