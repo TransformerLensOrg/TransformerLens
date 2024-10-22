28f5d7888774bb0590899600ee8e7e65e473620b
Bryce Meyer <bryce13950@gmail.com>
2024-10-22 02:03:17 +0200

    cleaned up config a bit

--- transformer_lens/loading_from_pretrained.py
+++ transformer_lens/loading_from_pretrained.py
@@ -933,44 +933,6 @@ def convert_hf_model_config(model_name: str, **kwargs):
             "final_rms": True,
             "gated_mlp": True,
         }
-    elif "Llama-3.2-1B-Instruct" in official_model_name:
-        cfg_dict = {
-            "d_model": 2048,
-            "d_head": 64,
-            "n_heads": 32,
-            "d_mlp": 8192,
-            "n_layers": 16,
-            "n_ctx": 2048,  # capped due to memory issues
-            "eps": 1e-5,
-            "d_vocab": 128256,
-            "act_fn": "silu",
-            "n_key_value_heads": 8,
-            "normalization_type": "RMS",
-            "positional_embedding_type": "rotary",
-            "rotary_adjacent_pairs": False,
-            "rotary_dim": 64,
-            "final_rms": True,
-            "gated_mlp": True,
-        }
-    elif "Llama-3.2-3B-Instruct" in official_model_name:
-        cfg_dict = {
-            "d_model": 3072,
-            "d_head": 128,
-            "n_heads": 24,
-            "d_mlp": 8192,
-            "n_layers": 28,
-            "n_ctx": 2048,  # capped due to memory issues
-            "eps": 1e-5,
-            "d_vocab": 128256,
-            "act_fn": "silu",
-            "n_key_value_heads": 8,
-            "normalization_type": "RMS",
-            "positional_embedding_type": "rotary",
-            "rotary_adjacent_pairs": False,
-            "rotary_dim": 128,
-            "final_rms": True,
-            "gated_mlp": True,
-        }
     elif "Llama-3.1-8B" in official_model_name:
         cfg_dict = {
             "d_model": 4096,
@@ -978,7 +940,7 @@ def convert_hf_model_config(model_name: str, **kwargs):
             "n_heads": 32,
             "d_mlp": 14336,
             "n_layers": 32,
-            "n_ctx": 2048,  # capped due to memory issues   #missing
+            "n_ctx": 2048,  # capped due to memory issues
             "eps": 1e-5,
             "d_vocab": 128256,
             "act_fn": "silu",
@@ -997,44 +959,6 @@ def convert_hf_model_config(model_name: str, **kwargs):
             "n_heads": 64,
             "d_mlp": 28672,
             "n_layers": 80,
-            "n_ctx": 2048,  # capped due to memory issues
-            "eps": 1e-5,
-            "d_vocab": 128256,
-            "act_fn": "silu",
-            "n_key_value_heads": 8,
-            "normalization_type": "RMS",
-            "positional_embedding_type": "rotary",
-            "rotary_adjacent_pairs": False,
-            "rotary_dim": 128,
-            "final_rms": True,
-            "gated_mlp": True,
-        }
-    elif "Llama-3.1-70B-Instruct" in official_model_name:
-        cfg_dict = {
-            "d_model": 8192,
-            "d_head": 128,
-            "n_heads": 64,
-            "d_mlp": 28672,
-            "n_layers": 80,
-            "n_ctx": 2048,  # capped due to memory issues
-            "eps": 1e-5,
-            "d_vocab": 128256,
-            "act_fn": "silu",
-            "n_key_value_heads": 8,
-            "normalization_type": "RMS",
-            "positional_embedding_type": "rotary",
-            "rotary_adjacent_pairs": False,
-            "rotary_dim": 128,
-            "final_rms": True,
-            "gated_mlp": True,
-        }
-    elif "Llama-3.1-8B-Instruct" in official_model_name:
-        cfg_dict = {
-            "d_model": 4096,
-            "d_head": 128,
-            "n_heads": 32,
-            "d_mlp": 14336,
-            "n_layers": 32,
             "n_ctx": 2048,  # capped due to memory issues
             "eps": 1e-5,
             "d_vocab": 128256,
