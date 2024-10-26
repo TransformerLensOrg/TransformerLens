cbb652cc2ec8559be8a10d38a360a33561eceed0
Hzfinfdu <zfhe00@gmail.com>
2024-10-26 12:27:45 +0800

    fix rope settings also for 3.2 models

--- transformer_lens/loading_from_pretrained.py
+++ transformer_lens/loading_from_pretrained.py
@@ -913,6 +913,11 @@ def convert_hf_model_config(model_name: str, **kwargs):
             "rotary_dim": 64,
             "final_rms": True,
             "gated_mlp": True,
+            "rotary_base": 500000.0,
+            "use_NTK_by_parts_rope": True,
+            "NTK_by_parts_low_freq_factor": 1.0,
+            "NTK_by_parts_high_freq_factor": 4.0,
+            "NTK_by_parts_factor": 32.0,
         }
     elif "Llama-3.2-3B" in official_model_name:
         cfg_dict = {
@@ -932,6 +937,11 @@ def convert_hf_model_config(model_name: str, **kwargs):
             "rotary_dim": 128,
             "final_rms": True,
             "gated_mlp": True,
+            "rotary_base": 500000.0,
+            "use_NTK_by_parts_rope": True,
+            "NTK_by_parts_low_freq_factor": 1.0,
+            "NTK_by_parts_high_freq_factor": 4.0,
+            "NTK_by_parts_factor": 32.0,
         }
     elif "Llama-3.1-8B" in official_model_name:
         cfg_dict = {
