9cf5279d315497a76718f66ff3f7cbec176c0bad
Bryce Meyer <bryce13950@gmail.com>
2024-12-24 01:22:01 +0100

    added more conversions

--- transformer_lens/weight_conversion/gemma.py
+++ transformer_lens/weight_conversion/gemma.py
@@ -28,33 +28,34 @@ def convert_gemma_weights(gemma, cfg: HookedTransformerConfig):
         #     # Only applies for Gemma 2
         #     state_dict[f"blocks.{l}.ln1_post.w"] = gemma.model.layers[
         #         l
-        #     ].post_attention_layernorm.weight.float() + torch.ones_like(
-        #         gemma.model.layers[l].input_layernorm.weight, dtype=torch.float32
-        #     )
+        # TODO Need to fix this so it pulls in the proper shape
+            ].post_attention_layernorm.weight.float() + torch.ones_like(
+                gemma.model.layers[l].input_layernorm.weight, dtype=torch.float32
+            )
 
         # W_Q = gemma.model.layers[l].self_attn.q_proj.weight
-        W_K = gemma.model.layers[l].self_attn.k_proj.weight
-        W_V = gemma.model.layers[l].self_attn.v_proj.weight
+        # W_K = gemma.model.layers[l].self_attn.k_proj.weight
+        # W_V = gemma.model.layers[l].self_attn.v_proj.weight
         # W_Q = einops.rearrange(W_Q, "(n h) m->n m h", n=cfg.n_heads)
-        W_K = einops.rearrange(W_K, "(n h) m->n m h", n=cfg.n_key_value_heads)
-        W_V = einops.rearrange(W_V, "(n h) m->n m h", n=cfg.n_key_value_heads)
+        # W_K = einops.rearrange(W_K, "(n h) m->n m h", n=cfg.n_key_value_heads)
+        # W_V = einops.rearrange(W_V, "(n h) m->n m h", n=cfg.n_key_value_heads)
         # state_dict[f"blocks.{l}.attn.W_Q"] = W_Q
-        state_dict[f"blocks.{l}.attn._W_K"] = W_K
-        state_dict[f"blocks.{l}.attn._W_V"] = W_V
+        # state_dict[f"blocks.{l}.attn._W_K"] = W_K
+        # state_dict[f"blocks.{l}.attn._W_V"] = W_V
 
-        state_dict[f"blocks.{l}.attn.b_Q"] = torch.zeros(cfg.n_heads, cfg.d_head, dtype=cfg.dtype)
-        state_dict[f"blocks.{l}.attn._b_K"] = torch.zeros(
-            cfg.n_key_value_heads, cfg.d_head, dtype=cfg.dtype
-        )
-        state_dict[f"blocks.{l}.attn._b_V"] = torch.zeros(
-            cfg.n_key_value_heads, cfg.d_head, dtype=cfg.dtype
-        )
+        # state_dict[f"blocks.{l}.attn.b_Q"] = torch.zeros(cfg.n_heads, cfg.d_head, dtype=cfg.dtype)
+        # state_dict[f"blocks.{l}.attn._b_K"] = torch.zeros(
+        #     cfg.n_key_value_heads, cfg.d_head, dtype=cfg.dtype
+        # )
+        # state_dict[f"blocks.{l}.attn._b_V"] = torch.zeros(
+        #     cfg.n_key_value_heads, cfg.d_head, dtype=cfg.dtype
+        # )
 
-        W_O = gemma.model.layers[l].self_attn.o_proj.weight
-        W_O = einops.rearrange(W_O, "m (n h)->n h m", n=cfg.n_heads)
-        state_dict[f"blocks.{l}.attn.W_O"] = W_O
+        # W_O = gemma.model.layers[l].self_attn.o_proj.weight
+        # W_O = einops.rearrange(W_O, "m (n h)->n h m", n=cfg.n_heads)
+        # state_dict[f"blocks.{l}.attn.W_O"] = W_O
 
-        state_dict[f"blocks.{l}.attn.b_O"] = torch.zeros(cfg.d_model, dtype=cfg.dtype)
+        # state_dict[f"blocks.{l}.attn.b_O"] = torch.zeros(cfg.d_model, dtype=cfg.dtype)
 
         # GemmaRMSNorm adds 1 to weights before multiplying by input, keep RMS calcs in float32
         if not cfg.use_normalization_before_and_after:
@@ -110,7 +111,6 @@ from transformer_lens.weight_conversion.conversion_utils.conversion_steps import
 
 class GemmalWeightConversion(ArchitectureConversion):
     def __init__(self, cfg: HookedTransformerConfig) -> None:
-        number_key_value_heads = cfg.n_key_value_heads if cfg.n_key_value_heads is not None else 0
 
         super().__init__(
             {
@@ -134,6 +134,7 @@ class GemmalWeightConversion(ArchitectureConversion):
         )
 
     def blocks_conversions(self, cfg: HookedTransformerConfig) -> WeightConversionSet:
+        number_key_value_heads = cfg.n_key_value_heads if cfg.n_key_value_heads is not None else 0
         if cfg.use_normalization_before_and_after:
             laynorm_conversion = self.normalization_before_and_after_conversions(cfg)
         else:
@@ -151,6 +152,22 @@ class GemmalWeightConversion(ArchitectureConversion):
                     "self_attn.q_proj.weight",
                     RearrangeWeightConversion("(n h) m->n m h", n=cfg.n_heads),
                 ),
+                "attn._W_K": (
+                    "self_attn.k_proj.weight",
+                    RearrangeWeightConversion("(n h) m->n m h", n=cfg.n_key_value_heads),
+                ),
+                "attn._W_V": (
+                    "self_attn.v_proj.weight",
+                    RearrangeWeightConversion("(n h) m->n m h", n=cfg.n_key_value_heads),
+                ),
+                "attn.W_O": (
+                    "self_attn.o_proj.weight",
+                    RearrangeWeightConversion("m (n h)->n h m", n=cfg.n_heads),
+                ),
+                "attn.b_Q": torch.zeros(cfg.n_heads, cfg.d_head, dtype=cfg.dtype),
+                "attn._b_K": torch.zeros(number_key_value_heads, cfg.d_head, dtype=cfg.dtype),
+                "attn._b_V": torch.zeros(number_key_value_heads, cfg.d_head, dtype=cfg.dtype),
+                "attn.b_O": torch.zeros(cfg.d_model, dtype=cfg.dtype),
             }
             | laynorm_conversion
         )
@@ -166,4 +183,11 @@ class GemmalWeightConversion(ArchitectureConversion):
         }
 
     def standard_normalization_conversions(self, cfg: HookedTransformerConfig) -> FIELD_SET:
-        return {}
+        return {
+            "ln2.w": (
+                "pre_feedforward_layernorm.weight",
+                CallableWeightConversion(
+                    lambda weight, model: weight.float() + torch.ones_like(model.norm.weight, dtype=torch.float32)
+                )
+            )
+        }
