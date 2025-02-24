2216b67adf73da49c7d4709467277f4d2cbadad7
Bryce Meyer <bryce13950@gmail.com>
2025-02-25 00:06:25 +0100

    fixed name

--- transformer_lens/hook_points.py
+++ transformer_lens/hook_points.py
@@ -341,10 +341,10 @@ class HookedRootModule(nn.Module):
             hook (Callable): The hook to add
             dir (Literal[&quot;fwd&quot;, &quot;bwd&quot;]): The direction for the hook
         """
-        module = self.mod_dict[name]
-        if not hasattr(module, "add_hook"):
+        hook_point_module = self.mod_dict[name]
+        if not hasattr(hook_point_module, "add_hook"):
             raise TypeError(f"Expected a module with add_hook, got {type(obj)}")
-        self.module.add_hook(hook, dir=dir, level=self.context_level)
+        hook_point_module.add_hook(hook, dir=dir, level=self.context_level)
 
     def _enable_hooks_for_points(
         self,
