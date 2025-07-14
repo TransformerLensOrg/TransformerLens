8d2ea4a72239911c1c6ebaf9680698451b7f5be4
Bryce Meyer <bryce13950@gmail.com>
2025-07-14 21:09:07 +0200

    ran format

--- transformer_lens/model_bridge/supported_architectures/llama.py
+++ transformer_lens/model_bridge/supported_architectures/llama.py
@@ -38,27 +38,19 @@ class LlamaArchitectureAdapter(ArchitectureAdapter):
                 "blocks.{i}.ln2.w": "model.layers.{i}.post_attention_layernorm.weight",
                 "blocks.{i}.attn.W_Q": (
                     "model.layers.{i}.self_attn.q_proj.weight",
-                    RearrangeWeightConversion(
-                        "(n h) m -> n m h", n=self.cfg.num_attention_heads
-                    ),
+                    RearrangeWeightConversion("(n h) m -> n m h", n=self.cfg.num_attention_heads),
                 ),
                 "blocks.{i}.attn.W_K": (
                     "model.layers.{i}.self_attn.k_proj.weight",
-                    RearrangeWeightConversion(
-                        "(n h) m -> n m h", n=self.cfg.num_attention_heads
-                    ),
+                    RearrangeWeightConversion("(n h) m -> n m h", n=self.cfg.num_attention_heads),
                 ),
                 "blocks.{i}.attn.W_V": (
                     "model.layers.{i}.self_attn.v_proj.weight",
-                    RearrangeWeightConversion(
-                        "(n h) m -> n m h", n=self.cfg.num_attention_heads
-                    ),
+                    RearrangeWeightConversion("(n h) m -> n m h", n=self.cfg.num_attention_heads),
                 ),
                 "blocks.{i}.attn.W_O": (
                     "model.layers.{i}.self_attn.o_proj.weight",
-                    RearrangeWeightConversion(
-                        "m (n h) -> n h m", n=self.cfg.num_attention_heads
-                    ),
+                    RearrangeWeightConversion("m (n h) -> n h m", n=self.cfg.num_attention_heads),
                 ),
                 "blocks.{i}.mlp.W_in": "model.layers.{i}.mlp.up_proj.weight.T",
                 "blocks.{i}.mlp.W_gate": "model.layers.{i}.mlp.gate_proj.weight.T",
