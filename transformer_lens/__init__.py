fcc7e73a631154fceb745c9a0ffaab2c55983826
Bryce Meyer <bryce13950@gmail.com>
2025-08-11 18:35:41 +0200

    restored init file

--- transformer_lens/__init__.py
+++ transformer_lens/__init__.py
@@ -1,36 +1,33 @@
-from . import (
-    components,
-    conversion_utils,
-    evals,
-    factories,
-    head_detector,
-    hook_points,
-    patching,
-    supported_models,
-    train,
-    utilities,
-)
-from . import loading_from_pretrained as loading
-from .ActivationCache import ActivationCache
-from .BertNextSentencePrediction import BertNextSentencePrediction
-from .FactoredMatrix import FactoredMatrix
-from .HookedEncoder import HookedEncoder
-from .HookedEncoderDecoder import HookedEncoderDecoder
-from .HookedTransformer import HookedTransformer
-from .HookedTransformer import HookedTransformer as EasyTransformer
-from .HookedTransformerConfig import HookedTransformerConfig
-from .HookedTransformerConfig import HookedTransformerConfig as EasyTransformerConfig
+from . import supported_models
+from . import utilities
+from . import hook_points
+from . import evals
+from . import conversion_utils
 from .past_key_value_caching import (
     HookedTransformerKeyValueCache,
     HookedTransformerKeyValueCacheEntry,
 )
+from . import components
+from . import factories
+from .HookedTransformerConfig import HookedTransformerConfig
+from .FactoredMatrix import FactoredMatrix
+from .ActivationCache import ActivationCache
+from .HookedTransformer import HookedTransformer
+from .SVDInterpreter import SVDInterpreter
+from .HookedEncoder import HookedEncoder
+from .HookedEncoderDecoder import HookedEncoderDecoder
+from .BertNextSentencePrediction import BertNextSentencePrediction
+from . import head_detector
+from . import loading_from_pretrained as loading
+from . import patching
+from . import train
+
 from .past_key_value_caching import (
     HookedTransformerKeyValueCache as EasyTransformerKeyValueCache,
-)
-from .past_key_value_caching import (
     HookedTransformerKeyValueCacheEntry as EasyTransformerKeyValueCacheEntry,
 )
-from .SVDInterpreter import SVDInterpreter
+from .HookedTransformer import HookedTransformer as EasyTransformer
+from .HookedTransformerConfig import HookedTransformerConfig as EasyTransformerConfig
 
 __all__ = [
     "HookedTransformerConfig",
