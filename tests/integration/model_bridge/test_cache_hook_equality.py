240553527f36a28a7d5481f88f2ca5558841513c
Bryce Meyer <bryce13950@gmail.com>
2025-09-10 18:14:30 +0200

    restored hooks

--- tests/integration/model_bridge/test_cache_hook_equality.py
+++ tests/integration/model_bridge/test_cache_hook_equality.py
@@ -27,7 +27,7 @@ act_names_in_cache = [
     # "hook_pos_embed",
     "blocks.0.hook_resid_pre",
     # "blocks.0.ln1.hook_scale",
-    # "blocks.0.ln1.hook_normalized",  # Temporarily disabled due to tolerance issues
+    "blocks.0.ln1.hook_normalized",
     # "blocks.0.attn.hook_q",
     # "blocks.0.attn.hook_k",
     # "blocks.0.attn.hook_v",
@@ -37,13 +37,13 @@ act_names_in_cache = [
     "blocks.0.hook_attn_out",
     "blocks.0.hook_resid_mid",
     # "blocks.0.ln2.hook_scale",
-    # "blocks.0.ln2.hook_normalized",  # Temporarily disabled due to tolerance issues
-    # "blocks.0.mlp.hook_pre",  # Disabled due to shape mismatch between implementations
+    "blocks.0.ln2.hook_normalized",
+    "blocks.0.mlp.hook_pre",
     # "blocks.0.mlp.hook_post",
     "blocks.0.hook_mlp_out",
     "blocks.0.hook_resid_post",
     # "ln_final.hook_scale",
-    # "ln_final.hook_normalized",  # Temporarily disabled due to tolerance issues
+    "ln_final.hook_normalized",
 ]
 
 
