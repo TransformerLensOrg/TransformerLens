0da6a7ca958c055fac566702c769ae580cc660d2
Jiankun Wei <72998341+david-wei-01001@users.noreply.github.com>
2026-04-17 14:52:19 -0400

    Update HookedVisualEncoder.py

--- transformer_lens/HookedVisualEncoder.py
+++ transformer_lens/HookedVisualEncoder.py
@@ -37,7 +37,7 @@ from transformer_lens.utilities import devices
 T = TypeVar("T", bound="HookedEncoder")
 
 
-class HookedEncoder(HookedRootModule):
+class HookedVisualEncoder(HookedRootModule):
     """
     This class implements a BERT-style encoder using the components in ./components.py, with HookPoints on every interesting activation. It inherits from HookedRootModule.
 
