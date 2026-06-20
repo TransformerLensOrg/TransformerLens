299bda7643d4199d3d7a7d410cdf13321720f193
huseyincavusbi <huseyincavus@proton.me>
2026-06-20 19:49:29 +0300

    fix: add Gemma4ForConditionalGeneration to MULTIMODAL_ARCHITECTURES

--- transformer_lens/utilities/architectures.py
+++ transformer_lens/utilities/architectures.py
@@ -33,6 +33,7 @@ MULTIMODAL_ARCHITECTURES: set[str] = {
     "LlavaNextForConditionalGeneration",
     "LlavaOnevisionForConditionalGeneration",
     "Gemma3ForConditionalGeneration",
+    "Gemma4ForConditionalGeneration",
     "Qwen3_5ForConditionalGeneration",
 }
 
