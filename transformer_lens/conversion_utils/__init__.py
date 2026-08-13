c77056fedbc88a05b4a01e677d01c50527cc78b5
Md.Sadiq <mohammadsadiq4950@gmail.com>
2026-08-14 00:06:27 +0530

    pipeline fix

--- transformer_lens/conversion_utils/__init__.py
+++ transformer_lens/conversion_utils/__init__.py
@@ -6,6 +6,6 @@ This module contains utilities for converting between different model architectu
 from transformer_lens.conversion_utils.conversion_steps import (
     TensorConversionSet,
 )
-from transformer_lens.conversion_utils.tl_checkpoint import convert_tl_checkpoint
+from transformer_lens.utilities.tl_checkpoint_conversion import convert_tl_checkpoint
 
 __all__ = ["TensorConversionSet", "convert_tl_checkpoint"]
